5cc07b4e8701739cae18c71c2659acc2bd38387d
unknown <bsa6.23.94@gmail.com>
2018-08-13 16:33:08 -0400

    initial commit of backend

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+mongoose 
+  .connect('f')//Whatever mongo db database we use will go here
+  .then(mongo => {
+    console.log('mongo server working')
+  })
+  .catch(err => {
+    console.log('error', err);
+  });
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'running' });
+  });
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
