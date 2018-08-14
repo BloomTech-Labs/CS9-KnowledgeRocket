c7e882163a157136a51d8cdb7d5b25f902ad6b35
kyle <kauzlarich.kyle@gmail.com>
2018-08-14 13:57:38 -0700

    Change Log added an asset folder for imgs changed the call to the dummy component in routes to the home component added in css for the home component home component added in place holders for main content and footer added in the sign in/up buttons and the banner image in the top section

--- client/src/components/routes/index.js
+++ client/src/components/routes/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Dummy from '../dummy/dummy';
+import Home from '../Home/Home';
 import NavBar from '../NavBar/NavBar';
 import ControlPanel from '../ControlPanel/ControlPanel';
 
