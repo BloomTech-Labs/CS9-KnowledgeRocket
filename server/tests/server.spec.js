3a7828dee135eb8a68a422fadf94fc5a72795cc5
unknown <bsa6.23.94@gmail.com>
2018-08-15 18:12:37 -0400

    added student functions to testing suite on the backend

--- server/tests/server.spec.js
+++ server/tests/server.spec.js
@@ -3,7 +3,8 @@ const request = require('supertest');
 const server = require('../server');
 const User = require('../user/User');
 const mongoose = require('mongoose');
-const testdb = process.env.TestDB_Url
+const Student = require('../student/Student');
+const testdb = process.env.TestDB_Url;
 
 describe("server", () => {
     beforeAll(() => {
@@ -29,6 +30,21 @@ describe("server", () => {
         expect(createdUser.email).toEqual(fakeMail);
         expect(response.status).toBe(201);
         mongoose.connection.db.dropCollection('users');
+        done();
+    });
+    test('should return 200 and a response', async (done) => {
+        const response = await request(server).get('/api/student');
+        expect(response.status).toBe(200);
+        done();
+    });
+    test('should return 201', async (done) => {
+        const fakeMail = 'bobtodd@gmail.com';
+        const mockUser = {firstName: 'bob', lastName: 'todd', email: 'bobtodd@gmail.com'};
+        const createdUser = await Student.create(mockUser);
+        const response = await request(server).post('/api/student').send(mockUser);
+        expect(createdUser.email).toEqual(fakeMail);
+        expect(response.status).toBe(201);
+        mongoose.connection.db.dropCollection('students');
         done();
     });
 })
