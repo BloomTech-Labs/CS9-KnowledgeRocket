f96c295d10dd24ff00064b8cabe45c3220edd046
kyle <kauzlarich.kyle@gmail.com>
2018-08-15 11:18:56 -0700

    Change Log removed the placeholder img tag from the .js and fixed the Home_Banner class to have the banner set as a background

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -9,12 +9,11 @@ export default class Home extends Component {
     render() {
         return (
             <div> {/*container*/}
-                <div className='HomeBanner'> {/* Top */}
+                <div className='Home_Banner'> {/* Top */}
                     <div className='Home_ButtonDiv'>
                         <button className='Home_Button' onClick={this.handleAuthRedirect}>Sign Up</button>
                         <button className='Home_Button' onClick={this.handleAuthRedirect}>Sign In</button>
                     </div>
-                    <img src={mainImg} alt='banner img' className='Home_Banner'></img>
                 </div> {/* Top End */}
                 <div className='Home_Content'> {/* Main Content*/}
                     <h1>What is a Knowledge Rocket?</h1>
