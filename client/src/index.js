ee409429fce616e7feacb168cc0e2a43e886603e
Moises Dobarganes <designerexpert@gmail.com>
2018-08-14 20:07:24 -0400

    Fixed Warnings

--- client/src/index.js
+++ client/src/index.js
@@ -5,18 +5,12 @@ import registerServiceWorker from './registerServiceWorker';
 // Redux Imports Here
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 // Components Imports Here
 import './index.css';
 import App from './App';
 
-// Apply Middleware to the Redux Store, to handle promises.
-// const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
-
-// // Create Store with Reducers and Redux Extention Support for Chrome Dev Tools.
-// const reduxStore = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const reduxStore = createStore(
     reducers,
     applyMiddleware(thunk)
