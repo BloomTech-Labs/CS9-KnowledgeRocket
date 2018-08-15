0ae8a981d3122050aba26a671c73c3d12a63a72d
JustinSHong <jhong2009@gmail.com>
2018-08-15 13:03:30 -0700

    added react-test-renderer

--- client/src/tests/AddButton.test.js
+++ client/src/tests/AddButton.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
 import AddButton from "../components/AddButton/AddButton.js";
 
 Enzyme.configure({ adapter: new Adapter() });
