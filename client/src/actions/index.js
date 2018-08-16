45220291fc841142c18f996bcad142331fca8ee0
Moises Dobarganes <designerexpert@gmail.com>
2018-08-16 19:04:41 -0400

    Figuring out merge issues

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
-
+import Firebase from 'firebase';
 // Set Up Back End URL: Change config for deployment or switch to ENV
 // process.env.server set to heroku deployment root, already set on deployed version.
 const url = process.env.REACT_APP_SERVER;
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIRE_API,
+    authDomain: process.env.REACT_APP_FIRE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIRE_DB_URL,
+    projectId: process.env.REACT_APP_FIRE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIRE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIRE_SENDER_ID,
+};
+
+Firebase.initializeApp(firebaseConfig);
 
 // Dummy Action Types
 export const ADD_ROCKET = 'ADD_ROCKET';
@@ -48,6 +60,114 @@ export const loginUser = user => async dispatch => {
         dispatch({ type: LOGIN_USER, payload: response.data });
     } catch (err) {
         dispatch({ type: LOGIN_USER_FAILURE });
+    }
+};
+
+// extract uid, email, token from response.user
+export const handleGoogleResponse = res => {
+    const token = res.credential.accessToken;
+    const { uid, email } = res.user;
+    console.log(`token ${token}`);
+    console.log(`user ${JSON.stringify(res.user)}`);
+    const user = {
+        uid: uid,
+        email: email,
+        token: token,
+        authType: '3rdParty',
+    };
+    return user;
+};
+
+// TODO SEND DATA TO AN ENDPOINT
+export const loginUserGoogle = () => async dispatch => {
+    const provider = new Firebase.auth.GoogleAuthProvider();
+    try {
+        let response = await Firebase.auth().signInWithPopup(provider);
+
+        const user = handleGoogleResponse(response);
+        return dispatch({
+            type: LOGIN_USER,
+            payload: {
+                uid: user.uid,
+                email: user.email,
+                token: user.token,
+            },
+        });
+    } catch (err) {
+        const errorCode = err.code;
+        const errorMessage = err.message;
+        console.log(errorCode, errorMessage);
+    }
+};
+
+export const handleFacebookResponse = res => {
+    const token = res.credential.accessToken;
+    const { uid, email } = res.user;
+    console.log(`token ${token}`);
+    console.log(`user ${JSON.stringify(res.user)}`);
+    const user = {
+        uid: uid,
+        email: email,
+        token: token,
+        authType: '3rdParty',
+    };
+    return user;
+};
+
+export const loginUserFacebook = () => async dispatch => {
+    const provider = new Firebase.auth.FacebookAuthProvider();
+
+    try {
+        let response = await Firebase.auth().signInWithPopup(provider);
+        console.log(`response ${JSON.stringify(response)}`);
+        const user = handleFacebookResponse(response);
+        return dispatch({
+            type: LOGIN_USER,
+            payload: {
+                uid: user.uid,
+                email: user.email,
+                token: user.token,
+            },
+        });
+    } catch (err) {
+        const errorCode = err.code;
+        const errorMessage = err.message;
+        console.log(errorCode, errorMessage);
+    }
+};
+
+export const handleTwitterResponse = res => {
+    const token = res.credential.accessToken;
+    const { uid, email } = res.user;
+    console.log(`token ${token}`);
+    console.log(`user ${JSON.stringify(res.user)}`);
+    const user = {
+        uid: uid,
+        email: email,
+        token: token,
+        authType: '3rdParty',
+    };
+    return user;
+};
+
+export const loginUserTwitter = () => async dispatch => {
+    const provider = new Firebase.auth.TwitterAuthProvider();
+
+    try {
+        let response = await Firebase.auth().signInWithPopup(provider);
+        const user = handleTwitterResponse(response);
+        return dispatch({
+            type: LOGIN_USER,
+            payload: {
+                uid: user.uid,
+                email: user.email,
+                token: user.token,
+            },
+        });
+    } catch (err) {
+        const errorCode = err.code;
+        const errorMessage = err.message;
+        console.log(errorCode, errorMessage);
     }
 };
 
--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { addUser, loginUser } from '../../actions';
+import {
+    addUser,
+    loginUser,
+    loginUserGoogle,
+    loginUserFacebook,
+    loginUserTwitter,
+} from '../../actions';
 import { Button, Input } from '@material-ui/core';
 import Progress from '../Progress/Progress';
 import './Auth.css';
-
-// Initialize Firebase
 
 function mapStateToProps(state) {
     return {
@@ -52,6 +56,18 @@ class Auth extends Component {
         this.setState({ attempts: this.state.attempts + 1 });
     };
 
+    handleSignInGoogle = e => {
+        this.props.loginUserGoogle();
+    };
+
+    handleSignInFacebook = e => {
+        this.props.loginUserFacebook();
+    };
+
+    handleSignInTwitter = e => {
+        this.props.loginUserTwitter();
+    };
+
     render() {
         // console.log('props user', this.props.user);
         // console.log('state user', this.state.authenticated);
@@ -90,6 +106,30 @@ class Auth extends Component {
                     >
                         Sign-In
                     </Button>
+                    <Button
+                        className="Auth_button"
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleSignInGoogle}
+                    >
+                        Log In with Google
+                    </Button>
+                    <Button
+                        className="Auth_button"
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleSignInFacebook}
+                    >
+                        Log In with Facebook
+                    </Button>
+                    <Button
+                        className="Auth_button"
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleSignInTwitter}
+                    >
+                        Log In with Twitter
+                    </Button>
                 </div>
                 <div className="flex-row-centered Auth_prompt-fail">
                     {/* THIS SECTION WILL HANDLE USER AUTH ERROR MESSAGES */}
@@ -111,5 +151,11 @@ class Auth extends Component {
 
 export default connect(
     mapStateToProps,
-    { addUser, loginUser }
+    {
+        addUser,
+        loginUser,
+        loginUserGoogle,
+        loginUserFacebook,
+        loginUserTwitter,
+    }
 )(Auth);
