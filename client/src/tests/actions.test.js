3bb5853c44c40707c77f1492c66224564f3fa91e
JustinSHong <jhong2009@gmail.com>
2018-08-16 13:32:30 -0700

    created test to make sure handleGoogleResponse() returns the correct action and payload

---
+++ client/src/tests/actions.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+// Actions
+import { handleGoogleResponse } from '../actions';
+
+const mockStore = configureStore();
+const store = mockStore();
+
+describe('action creators', () => {
+	describe('handleGoogleResponse', () => {
+		// clear all actions from mock
+		beforeEach(() => {
+			store.clearActions();
+		});
+
+		it('should dispatch the correct action and payload', async () => {
+			const expected = {
+				uid: 'abc',
+				email: 'johndoe@yahoo.com',
+				token: '123abc',
+				authType: '3rdParty',
+			};
+
+			const response = {
+				credential: {
+					accessToken: '123abc',
+				},
+				user: {
+					uid: 'abc',
+					email: 'johndoe@yahoo.com',
+				},
+			};
+			expect(handleGoogleResponse(response)).toEqual(expected);
+		});
+	});
+});
