91479fb24ab0ad4bc2aaf7e8fa495f3dbe8023ef
JustinSHong <jhong2009@gmail.com>
2018-08-15 22:50:55 -0700

    imported loginUserFacebook from actions

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import firebase from 'firebase';
 import { Redirect } from 'react-router-dom';
-import { addUser, loginUser, loginUserGoogle } from '../../actions';
+import { addUser, loginUser, loginUserGoogle, loginUserFacebook } from '../../actions';
 import { Button, Input } from '@material-ui/core';
 import './Auth.css';
 
@@ -116,4 +116,6 @@ class Auth extends Component {
     }
 }
 
-export default connect(mapStateToProps, { addUser, loginUser, loginUserGoogle, loginUserFacebook })(Auth);
+export default connect(mapStateToProps, { addUser, loginUser, loginUserGoogle, loginUserFacebook })(
+    Auth
+);
