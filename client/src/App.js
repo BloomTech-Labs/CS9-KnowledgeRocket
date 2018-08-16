e3daaf2a62847c2c3fcd8437bf9335a3bec781d6
JustinSHong <jhong2009@gmail.com>
2018-08-15 18:01:53 -0700

    fixed import issues involving Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Routes from "./components/Routes/Routes";
+import Routes from "./components/Routes/";
 
 class App extends Component {
   render() {
