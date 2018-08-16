89c32a1bc22fa10af22fd1056faf5f88872e4e44
JustinSHong <jhong2009@gmail.com>
2018-08-15 17:29:25 -0700

    removed empty test files

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -4,7 +4,7 @@ import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 // Components
 import App from "../App";
-import Routes from "../components/Routes";
+import Routes from "../components/Routes/Routes.js";
 
 Enzyme.configure({ adapter: new Adapter() });
 
