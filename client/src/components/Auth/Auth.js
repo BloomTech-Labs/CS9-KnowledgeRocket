ffac9e676a3c7a9569613e599cc0385b3689710d
JustinSHong <jhong2009@gmail.com>
2018-08-15 19:26:36 -0700

    created log in with google btn and handleSignInGoogle() click handler

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import firebase from "firebase";
+import firebase from 'firebase';
 import { Redirect } from 'react-router-dom';
-import { addUser, loginUser } from '../../actions';
+import { addUser, loginUser, loginUserGoogle } from '../../actions';
 import { Button, Input } from '@material-ui/core';
 import './Auth.css';
 
@@ -12,7 +12,7 @@ firebase.initializeApp(FIREBASE_CONFIG);
 
 function mapStateToProps(state) {
     return {
-        user: state.user
+        user: state.user,
     };
 }
 
@@ -20,54 +20,93 @@ class Auth extends Component {
     state = {
         email: '',
         password: '',
-        authenticated: {}
-    }
+        authenticated: {},
+    };
 
-    componentDidMount() {
-    }
+    componentDidMount() {}
 
-    handleInput = (e) => {
+    handleInput = e => {
         this.setState({ [e.target.name]: e.target.value });
-    }
-    
-    handleSignUp = (e) => {
-        const user = {
-            email: this.state.email,
-            password: this.state.password,
-            authType: "signup"
-        }
-        this.props.addUser(user);
-    }
+    };
 
-    handleSignIn = (e) => {
+    handleSignUp = e => {
         const user = {
             email: this.state.email,
             password: this.state.password,
-            authType: "signin"
-        }
+            authType: 'signup',
+        };
+        this.props.addUser(user);
+    };
+
+    handleSignIn = e => {
+        const user = {
+            email: this.state.email,
+            password: this.state.password,
+            authType: 'signin',
+        };
         this.props.loginUser(user);
-    }
+    };
+
+    handleSignInGoogle = e => {
+        // const user = {
+        //     email: this.state.email,
+        //     password: this.state.password,
+        //     authType: 'signin',
+        // };
+        this.props.loginUserGoogle();
+    };
 
     render() {
-        console.log('props user',this.props.user)
-        console.log('state user', this.state.authenticated)
+        console.log('props user', this.props.user);
+        console.log('state user', this.state.authenticated);
         return (
-            <div className='Main_container'>
-                <h1 className='Auth_header'>Please Sign-in or Sign-up.</h1>
-                <div className='flex-column-centered'>
-                    <Input className='Auth_input' type='email' name='email' autoFocus={true} onChange={this.handleInput} />
-                    <Input className='Auth_input' type='password' name='password' onChange={this.handleInput} />
+            <div className="Main_container">
+                <h1 className="Auth_header">Please Sign-in or Sign-up.</h1>
+                <div className="flex-column-centered">
+                    <Input
+                        className="Auth_input"
+                        type="email"
+                        name="email"
+                        autoFocus={true}
+                        onChange={this.handleInput}
+                    />
+                    <Input
+                        className="Auth_input"
+                        type="password"
+                        name="password"
+                        onChange={this.handleInput}
+                    />
                 </div>
-                <div className='flex-row-centered'>
-                    <Button className='Auth_button' variant="contained" color="primary" onClick={this.handleSignUp}>Sign-Up</Button>
-                    <Button className='Auth_button' variant="contained" color="primary" onClick={this.handleSignIn}>Sign-In</Button>
+                <div className="flex-row-centered">
+                    <Button
+                        className="Auth_button"
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleSignUp}
+                    >
+                        Sign-Up
+                    </Button>
+                    <Button
+                        className="Auth_button"
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleSignIn}
+                    >
+                        Sign-In
+                    </Button>
+                    <Button
+                        className="Auth_button"
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleSignInGoogle}
+                    >
+                        Log In with Google
+                    </Button>
                 </div>
-                {this.props.user.authenticated ? <Redirect to='/rocket' /> : null}
+                {this.props.user.authenticated ? <Redirect to="/rocket" /> : null}
             </div>
         );
     }
 }
 
-export default connect(
-    mapStateToProps, { addUser, loginUser}
-)(Auth);
+export default connect(mapStateToProps, { addUser, loginUser, loginUserGoogle })(Auth);
