2ff85fc1bf83d4027bed75ce86a40b03e5bb8600
JustinSHong <jhong2009@gmail.com>
2018-08-16 13:54:29 -0700

    ?

--- client/src/components/Cohort/Cohort.js
+++ client/src/components/Cohort/Cohort.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './Cohort.css';
+// Components
+import CohortSettingForm from '../CohortSettingForm/CohortSettingForm.js';
+// Material Components
+import Button from '@material-ui/core/Button';
 
 function mapStateToProps(state) {
-    return {
-        state
-    };
+  return {
+    state,
+  };
 }
 
 class Cohort extends Component {
-    componentDidMount() {
-        // Checks for User to be Authenticated
-        // If not authenticated it will send the user to <login/>
-        // If authenticated it will set the state with the current user.
-        if (!this.props.state.user.authenticated) {
-            this.props.history.push('/rocket/auth');
-        }
+  componentDidMount() {
+    // Checks for User to be Authenticated
+    // If not authenticated it will send the user to <login/>
+    // If authenticated it will set the state with the current user.
+    if (!this.props.state.user.authenticated) {
+      this.props.history.push('/rocket/auth');
     }
-    render() {
-        return (
-            <div className='Main_container'>
-                COHORT PLACEHOLDER
-            </div>
-        );
-    }
+  }
+  render() {
+    return (
+      <div className="Main_Container">
+        <Button color="primary">Sign Up</Button>
+        <CohortSettingForm />
+      </div>
+    );
+  }
 }
 
-export default connect(
-    mapStateToProps,
-)(Cohort);
+export default connect(mapStateToProps)(Cohort);
--- client/src/tests/actions.test.js
+++ client/src/tests/actions.test.js
@@ -3,26 +3,22 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 // Actions
-import { handleGoogleResponse } from '../actions';
+import { handleGoogleResponse, handleTwitterResponse } from '../actions';
 
 const mockStore = configureStore();
 const store = mockStore();
 
 describe('action creators', () => {
 	describe('handleGoogleResponse', () => {
-		// clear all actions from mock
-		beforeEach(() => {
-			store.clearActions();
-		});
-
 		it('should create a user object from a response object', async () => {
+			// describes a particular user
 			const expected = {
 				uid: 'abc',
 				email: 'johndoe@yahoo.com',
 				token: '123abc',
 				authType: '3rdParty',
 			};
-
+			// describes response object from async call to firebase.auth()
 			const response = {
 				credential: {
 					accessToken: '123abc',
@@ -33,6 +29,29 @@ describe('action creators', () => {
 				},
 			};
 			expect(handleGoogleResponse(response)).toEqual(expected);
+		});
+	});
+
+	describe('handleTwitterResponse', () => {
+		it('should create a user object from a response object', async () => {
+			const expected = {
+				uid: 'def',
+				email: 'janedoe@yahoo.com',
+				token: '456def',
+				authType: '3rdParty',
+			};
+			// describes response object from async call to firebase.auth()
+			const response = {
+				credential: {
+					accessToken: '456def',
+				},
+				user: {
+					uid: 'def',
+					email: 'janedoe@yahoo.com',
+				},
+			};
+
+			expect(handleTwitterResponse(response)).toEqual(expected);
 		});
 	});
 });
