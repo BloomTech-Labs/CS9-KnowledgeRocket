137d99b0e5003b7cb32d49c399d95da60ae5e2f6
JustinSHong <jhong2009@gmail.com>
2018-08-17 10:56:13 -0700

    added /rocket/single/:id to render Rocket, changed /rocket to render RocketList

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -1,21 +1,25 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Home from "../Home/Home";
-import NavBar from "../NavBar/NavBar";
-import ControlPanel from "../ControlPanel/ControlPanel";
-import Auth from "../Auth/Auth";
-import Rocket from "../Rocket/Rocket";
-import Billing from "../Billing/Billing";
-import Cohort from "../Cohort/Cohort";
-import Settings from "../Settings/Settings";
+import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+// Components
+import Home from '../Home/Home';
+import NavBar from '../NavBar/NavBar';
+import ControlPanel from '../ControlPanel/ControlPanel';
+import Auth from '../Auth/Auth';
+import Rocket from '../Rocket/Rocket';
+import RocketList from '../RocketList/RocketList';
+import Billing from '../Billing/Billing';
+import Cohort from '../Cohort/Cohort';
+import Settings from '../Settings/Settings';
 
 export default () => (
     <BrowserRouter>
         <div className="routeContainer">
             <Route path="/rocket" component={NavBar} />
             <div className="Content_container">
+                {/* <Route path="/" component={Home} /> */}
                 <Route path="/rocket" component={ControlPanel} />
-                <Route path="/rocket" exact component={Rocket} />
+                <Route path="/rocket" exact component={RocketList} />
+                <Route path="/rocket/single/:id" exact component={Rocket} />
                 <Route path="/rocket/billing" exact component={Billing} />
                 <Route path="/rocket/classes" exact component={Cohort} />
                 <Route path="/rocket/settings" exact component={Settings} />
