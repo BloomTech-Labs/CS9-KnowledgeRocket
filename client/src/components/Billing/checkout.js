33707f15516cd4c05da8affa19288c188408cbfe
unknown <bsa6.23.94@gmail.com>
2018-08-17 18:47:18 -0400

    some messup removed mongourl put it back

--- client/src/components/Billing/checkout.js
+++ client/src/components/Billing/checkout.js
@@ -2,23 +2,20 @@ import React, { Component } from 'react';
 import { CardElement, injectStripe } from 'react-stripe-elements';
 import axios from 'axios';
 const mongoURL = process.env.REACT_APP_Db_Url || 'mongodb://127.0.0.1/knowledgerocket';
-
 class CheckoutForm extends Component {
     constructor(props) {
         super(props);
         this.state = { complete: false };
         this.submit = this.submit.bind(this);
     }
-
     async submit(ev) {
         let { token } = await this.props.stripe.createToken({ name: 'Name' });
-        let response = await axios.post(``, {
+        let response = await axios.post(`${mongoURL}`, {
             token: token.id,
             uid: this.props.uid,
         });
         if (response) this.setState({ complete: true });
     }
-
     render() {
         if (this.state.complete) return <h1>Purchase Complete!!</h1>;
         return (
@@ -30,5 +27,4 @@ class CheckoutForm extends Component {
         );
     }
 }
-
 export default injectStripe(CheckoutForm);
