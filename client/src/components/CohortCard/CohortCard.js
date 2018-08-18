b98f9d72ad9b0b06b20e1958ab2c0fd613fe637a
JustinSHong <jhong2009@gmail.com>
2018-08-17 17:02:01 -0700

    set up CohortCard to display cohort name, number of students, participation, and rocket count

--- client/src/components/CohortCard/CohortCard.js
+++ client/src/components/CohortCard/CohortCard.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './CohortCard.css'
+// Material Components
+import CardContent from '@material-ui/core/CardContent';
+import Input from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+// Styles
+import './CohortCard.css';
 
-function mapStateToProps(state) {
-    return {
+// function mapStateToProps(state) {
+//     return {
 
-    };
-}
+//     };
+// }
 
+// RENDERS A COHORT CARD FOR A SINGLE CLASS - receives class info from CohortList
 class CohortCard extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+	render() {
+		// each card renders:
+		// Cohort name
+		// Number of students
+		// Participation(%)
+		// Rockets sent
+		return (
+			<CardContent>
+				<FormControl>
+					<Input>Name:</Input>
+					<Input>Number of Students:</Input>
+					<Input>Participation:</Input>
+					<Input>Rockets Sent:</Input>
+				</FormControl>
+			</CardContent>
+		);
+	}
 }
 
-export default connect(
-    mapStateToProps,
-)(CohortCard);
+export default CohortCard;
+// export default connect(mapStateToProps)(CohortCard);
