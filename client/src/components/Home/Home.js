21daba82d4c352947d659794144134500e3ab590
Moises Dobarganes <designerexpert@gmail.com>
2018-08-18 20:15:21 -0400

    Implemented Landing Page Footer and Nav: All components are now Styled Components Importing components from HomeStyled.js file TODO: Integrate the StyledComponents into testing. TODO: Integrate the whole LandingPage into testing.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,128 +1,99 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import './index.css';
-import Styled from 'styled-components';
-
-const HomeCTA = Styled.div`
-    font-family: 'Roboto';
-    background: url('/img/HomePage_CTA_07.svg') no-repeat center center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    min-height: 1150px;
-    background-color: black;
-    @media(min-width: 2000px) {
-        &{
-            background-size: contain;
-        }
-    }
-`;
-
-export const HomeContainer = Styled.div`
-    font-family: 'Roboto', sans-serif;
-`;
-
-const MidContainer = Styled.div`
-    font-family: 'Roboto', sans-serif;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
-const HomeHeader = Styled.h2`
-    font-size: 4rem;
-    font-weight: 900;
-    color: #232323;
-    min-width: 300px;
-    max-width: 800px;
-    text-align: center;
-    margin: 3rem 0;
-`;
-
-const MidSectionWrapper = Styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-    width: 100%;
-`;
-
-const MidSection = Styled.div`
-    display: flex;
-    flex-direction: column;
-    min-width: 300px;
-    max-width: 30%;
-    background-color: #DFDFDF;
-    border-radius: .5rem;
-    border: 1px solid #232323;;
-    margin: 1rem;
-    box-shadow: 0px 0px 12px 0px rgba(0,0,0,0.5)
-`;
-
-const MidImg = Styled.img`
-    background: url( ${(props) => { return props.src }} );
-    margin: 1rem;
-    height: 200px;
-    background-color: #FEFEFE;
-    border-radius: .3rem;
-`;
-
-const MidSectionText = Styled.p`
-    margin: 1rem;
-    font-size: 1.5rem;
-    text-align: justify;
-`;
+import Logo from '../../assets/Logo_BW_SML.svg';
+import {
+    HomeContainer,
+    HomeCTA,
+    HomeNav,
+    NavLogo,
+    NavButton,
+    HomeHeader,
+    MidSection,
+    MidSectionWrapper,
+    MidColumn,
+    MidContainer,
+    MidHeader,
+    MidImg,
+    MidSectionText,
+    FooterSection,
+} from './HomeStyled';
 
 export default class Home extends Component {
     handleAuthRedirect = e => {
         this.props.history.push('/rocket/auth');
     };
+
+    handleHome = e => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    };
+
+    handleContact = e => {
+        window.location.href = 'mailto:lambdaknowledgerocket@gmail.com';
+    };
+
     render() {
-        return <HomeContainer>
+        return (
+            <HomeContainer>
                 <HomeCTA>
-                    <div className="Home_ButtonDiv">
-                        <Button className="Home_Button" color="primary" onClick={this.handleAuthRedirect}>
-                            Sign Up
-                        </Button>
-                        <Button className="Home_Button" color="primary" onClick={this.handleAuthRedirect}>
-                            Sign In
-                        </Button>
-                    </div>
+                    <HomeNav>
+                        <div>
+                            <NavLogo src={Logo} onClick={this.handleHome} />
+                        </div>
+                        <div>
+                            <NavButton onClick={this.handleAuthRedirect}>Sign Up</NavButton>
+                            <NavButton onClick={this.handleAuthRedirect}>Sign In</NavButton>
+                        </div>
+                    </HomeNav>
                 </HomeCTA>
                 <MidContainer>
                     <HomeHeader>What is a Knowledge Rocket?</HomeHeader>
+
                     <MidSectionWrapper>
-                        <MidSection>
-                            <MidImg src="/img/amanda-sandlin-10508-unsplash.jpg" />
-                            <MidSectionText>
-                                Text for mid section here should not be very length but
-                                should contain enough information about this section.Text
-                                for mid section here should not be very length but should
-                                contain enough information about this section.
-                            </MidSectionText>
-                        </MidSection>
-                        <MidSection>
-                            <MidImg src="/img/erik-nielsen-783260-unsplash.jpg" />
-                            <MidSectionText>
-                                Text for mid section here should not be very length but
-                                should contain enough information about this section.Text
-                                for mid section here should not be very length but should
-                                contain enough information about this section.Text for mid
-                                section here should not be very length but should contain
-                                enough information about this section.
-                            </MidSectionText>
-                        </MidSection>
-                        <MidSection>
-                        <MidImg src="/img/nasa-63029-unsplash.jpg" />
-                            <MidSectionText>
-                                Text for mid section here should not be very length but
-                                should contain enough information about this section.Text
-                                for mid section here should not be very length but should
-                                contain enough information about this section.
-                            </MidSectionText>
-                        </MidSection>
+                        <MidColumn>
+                            <MidHeader>Implementation</MidHeader>
+                            <MidSection>
+                                <MidImg src="/img/amanda-sandlin-10508-unsplash.jpg" />
+                                <MidSectionText>
+                                    Text for mid section here should not be very length but should
+                                    contain enough information about this section.Text for mid
+                                    section here should not be very length but should contain enough
+                                    information about this section.
+                                </MidSectionText>
+                            </MidSection>
+                        </MidColumn>
+                        <MidColumn>
+                            <MidHeader>Practice</MidHeader>
+                            <MidSection>
+                                <MidImg src="/img/erik-nielsen-783260-unsplash.jpg" />
+                                <MidSectionText>
+                                    Text for mid section here should not be very length but should
+                                    contain enough information about this section.Text for mid
+                                    section here should not be very length but should contain enough
+                                    information about this section.Text for mid section here should
+                                    not be very length but should contain enough information about
+                                    this section.
+                                </MidSectionText>
+                            </MidSection>
+                        </MidColumn>
+                        <MidColumn>
+                            <MidHeader>Results</MidHeader>
+                            <MidSection>
+                                <MidImg src="/img/nasa-63029-unsplash.jpg" />
+                                <MidSectionText>
+                                    Text for mid section here should not be very length but should
+                                    contain enough information about this section.Text for mid
+                                    section here should not be very length but should contain enough
+                                    information about this section.
+                                </MidSectionText>
+                            </MidSection>
+                        </MidColumn>
                     </MidSectionWrapper>
                 </MidContainer>
-            </HomeContainer>;
+                <FooterSection>
+                    <NavButton onClick={this.handleContact}>CONTACT US</NavButton>
+                    <NavButton onClick={this.handleAuthRedirect}>SIGN UP</NavButton>
+                    <NavButton onClick={this.handleAuthRedirect}>SIGN IN</NavButton>
+                </FooterSection>
+            </HomeContainer>
+        );
     }
 }
---
+++ client/src/components/Home/HomeStyled.js
@@ -0,0 +1,122 @@
+import Button from '@material-ui/core/Button';
+import Styled from 'styled-components';
+
+export const HomeCTA = Styled.div`
+    font-family: 'Roboto';
+    background: url('/img/HomePage_CTA_07.svg') no-repeat center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    min-height: 1150px;
+    background-color: black;
+    @media(min-width: 2000px) {
+        &{
+            background-size: contain;
+        }
+    }
+`;
+
+export const HomeNav = Styled.div`
+    display: flex;
+    justify-content: space-between;
+    position: fixed;
+    width: 100%;
+    background-color: black;
+`;
+
+export const NavButton = Styled(Button)`
+    background-color: white !important;
+    color: black !important;
+    font-weight: 900 !important;
+    margin: .4rem !important;
+`;
+
+export const NavLogo = Styled.img`
+    height: 3rem;
+    width: 3rem;
+    margin: 0 1rem;
+`;
+
+export const HomeContainer = Styled.div`
+    font-family: 'Roboto', sans-serif;
+`;
+
+export const MidContainer = Styled.div`
+    font-family: 'Roboto', sans-serif;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const HomeHeader = Styled.h2`
+    font-size: 4rem;
+    font-weight: 900;
+    color: #232323;
+    min-width: 300px;
+    max-width: 800px;
+    text-align: center;
+    margin: 3rem 0;
+`;
+
+export const MidHeader = Styled.h3`
+    font-size: 2rem;
+    font-weight: 900;
+    color: #232323;
+    min-width: 300px;
+    max-width: 800px;
+    text-align: center;
+    margin: 3rem 0;
+`;
+
+export const MidSectionWrapper = Styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+    justify-content: space-evenly;
+    margin: 0 0 2rem 0;
+`;
+
+export const MidColumn = Styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 410px;
+    min-width: 300px;
+`;
+
+export const MidSection = Styled.div`
+    display: flex;
+    flex-direction: column;
+    min-width: 300px;
+    max-width: 100%;
+    background-color: #DFDFDF;
+    border-radius: .5rem;
+    border: 1px solid #232323;;
+    margin: 1rem;
+    min-height: 80vh;
+    box-shadow: 0px 0px 12px 0px rgba(0,0,0,0.5);
+`;
+
+export const MidImg = Styled.img`
+    background: url( ${props => {
+        return props.src;
+    }} );
+    margin: 1rem;
+    height: 200px;
+    background-color: #FEFEFE;
+    border-radius: .3rem;
+`;
+
+export const MidSectionText = Styled.p`
+    margin: 1rem;
+    font-size: 1.5rem;
+    text-align: justify;
+`;
+
+export const FooterSection = Styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    background-color: black;
+`;
