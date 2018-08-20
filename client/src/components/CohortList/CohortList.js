047dbd45661ff22ff1a044ff8e695450d755013a
JustinSHong <jhong2009@gmail.com>
2018-08-20 09:39:54 -0700

    CohortList.js: changed div to CohortListContainer styled component

--- client/src/components/CohortList/CohortList.js
+++ client/src/components/CohortList/CohortList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 // import { connect } from 'react-redux';
 // Material Components
 import Card from '@material-ui/core/Card';
@@ -16,60 +17,72 @@ import './CohortList.css';
 //     };
 // }
 
+const CohortListContainer = styled.div`
+  margin-left: 1rem;
+  padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-around;
+  width: 100%;
+  border: 1px solid rgb(119, 136, 153);
+  border-radius: 0.4rem;
+`;
+
 // RENDERS A LIST OF COHORT CARDS
 class CohortList extends Component {
-    state = {
-        cohorts: [{}],
-    };
+  state = {
+    cohorts: [{}],
+  };
 
-    // pull cohort list for a user from state this.props.state.user.cohorts
-    componentDidMount() {
-        // Checks for User to be Authenticated
-        // If not authenticated it will send the user to <login/>
-        // If authenticated it will set the state with the current user.
-        // if (!this.props.state.user.authenticated) {
-        //     this.props.history.push('/rocket/auth');
-        // }
-    }
-    render() {
-        return (
-            <div className="CohortListContainer">
-                {this.state.cohorts.length > 0
-                    ? [
-                          <Card className="CohortCard" key={1}>
-                              <CohortCard />
-                          </Card>,
-                          <Card className="CohortCard" key={2}>
-                              <CohortCard />
-                          </Card>,
-                          <Card className="CohortCard" key={3}>
-                              <CohortCard />
-                          </Card>,
-                          <Card className="CohortCard" key={4}>
-                              <CohortCard />
-                          </Card>,
-                          <Card className="AddButtonCard" key={5}>
-                              <CardContent className="CohortCard_AddBtn">
-                                  <p>New Class</p>
-                                  <Button variant="fab" color="primary">
-                                      <AddIcon />
-                                  </Button>
-                              </CardContent>
-                          </Card>,
-                      ]
-                    : [
-                          <Card className="AddButtonCard">
-                              <CardContent className="CohortCard_AddBtn">
-                                  <p>Add a new class</p>
-                                  <Button variant="fab" color="primary">
-                                      <AddIcon />
-                                  </Button>
-                              </CardContent>
-                          </Card>,
-                      ]}
-            </div>
-        );
-    }
+  // pull cohort list for a user from state this.props.state.user.cohorts
+  componentDidMount() {
+    // Checks for User to be Authenticated
+    // If not authenticated it will send the user to <login/>
+    // If authenticated it will set the state with the current user.
+    // if (!this.props.state.user.authenticated) {
+    //     this.props.history.push('/rocket/auth');
+    // }
+  }
+  render() {
+    return (
+      <CohortListContainer>
+        {this.state.cohorts.length > 0
+          ? [
+              <Card className="CohortCard" key={1}>
+                <CohortCard />
+              </Card>,
+              <Card className="CohortCard" key={2}>
+                <CohortCard />
+              </Card>,
+              <Card className="CohortCard" key={3}>
+                <CohortCard />
+              </Card>,
+              <Card className="CohortCard" key={4}>
+                <CohortCard />
+              </Card>,
+              <Card className="AddButtonCard" key={5}>
+                <CardContent className="CohortCard_AddBtn">
+                  <p>New Class</p>
+                  <Button variant="fab" color="primary">
+                    <AddIcon />
+                  </Button>
+                </CardContent>
+              </Card>,
+            ]
+          : [
+              <Card className="AddButtonCard">
+                <CardContent className="CohortCard_AddBtn">
+                  <p>Add a new class</p>
+                  <Button variant="fab" color="primary">
+                    <AddIcon />
+                  </Button>
+                </CardContent>
+              </Card>,
+            ]}
+      </CohortListContainer>
+    );
+  }
 }
 
 // export default connect(mapStateToProps)(CohortList);
