6659ba79dfdc9fe3c9ef4a53b8cf7574f2ec103c
axhon <axhonruiz@gmail.com>
2018-08-20 13:26:13 -1000

    test

--- server/mail/MailModel.spec.js
+++ server/mail/MailModel.spec.js
@@ -3,4 +3,8 @@ describe('MailModel', () => {
     it('has a getAllCohorts fn', () => {
         expect(getAllCohorts).toBeTruthy();
     });
+
+    it('returns data', async () => {
+        expect(await getAllCohorts()).toBeTruthy();
+    });
 });
