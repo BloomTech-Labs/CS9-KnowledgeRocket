4773dc86cb5826b5673ca4187228dbf00bc965b3
unknown <bsa6.23.94@gmail.com>
2018-08-21 13:22:08 -0400

    removed user from server as that functionality is in billing router

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ const RocketRouter = require('./rocket/RocketRouter');
 const ResponseRocketRouter = require('./responserocket/ResponseRocketRouter');
 const QuestionRouter = require('./question/QuestionRouter');
 const CohortRouter = require('./cohort/CohortRouter');
-const User = require('./user/User');
 
 const server = express();
 
@@ -37,50 +36,5 @@ server.use('/api/user', authMiddleware, UserRouter);
 server.use('/api/responserocket', authMiddleware, ResponseRocketRouter);
 server.use('/api/question', authMiddleware, QuestionRouter);
 server.use('/api/cohort', authMiddleware, CohortRouter);
-
-//Stripe Stuff
-server.post('/charge/:type', async (req, res) => {
-    const { type } = req.params;
-    const cost = type === 'monthly' ? 999 : 2999;
-    try {
-        let { status } = await stripe.charges.create({
-            amount: cost,
-            currency: 'usd',
-            description: `Subscription Charge for: ${type}`,
-            source: req.body.token,
-        });
-        //In here modify users to switch between pro and free
-        if (status) {
-            User.findById(req.body.id).then(currentUser => {
-                let expirationDate = Date.parse(currentUser.expiration);
-                let year =
-                    expirationDate > Date.now()
-                        ? expirationDate + 365 * 24 * 60 * 60 * 1000
-                        : Date.now() + 365 * 24 * 60 * 60 * 1000;
-                let month =
-                    expirationDate > Date.now()
-                        ? expirationDate + 30 * 24 * 60 * 60 * 1000
-                        : Date.now() + 30 * 24 * 60 * 60 * 1000;
-                console.log(year, month);
-                const newExpiration = type === 'monthly'? month: year;
-                User.findByIdAndUpdate(req.body.id, {
-                    account: type,
-                    expiration: newExpiration,
-                })
-                    .then(found => {
-                        console.log(found);
-                        res.status(201).json({ status });
-                    })
-                    .catch(err => {
-                        res.status(500).json(err);
-                    });
-            });
-        } else {
-            res.status(500).json({ status });
-        }
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
 
 module.exports = server;
