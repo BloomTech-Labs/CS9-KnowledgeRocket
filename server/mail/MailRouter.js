08ba3d11a563440be7e6ac4b9bb2a41dfbf3c5df
axhon <axhonruiz@gmail.com>
2018-08-20 14:58:40 -1000

    async await works in express

--- server/mail/MailRouter.js
+++ server/mail/MailRouter.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
 const { getAllCohorts } = require('./MailModel');
 
-router.route('/').get((req, res) => {
-    getAllCohorts()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.json({
-                err,
-            });
+router.route('/').get(async (req, res) => {
+    try {
+        const data = await getAllCohorts();
+        res.json(data);
+    } catch (err) {
+        res.json({
+            err,
         });
+    }
 });
 
 module.exports = router;
