98a1faff09cb699e4a7572a1e75faf1a1a512ad3
JustinSHong <jhong2009@gmail.com>
2018-08-21 10:20:00 -0700

    CohortList.test.js - added test statements

--- client/src/tests/CohortList.test.js
+++ client/src/tests/CohortList.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import { createStore } from 'redux';
 import reducer from '../reducers';
 import { BrowserRouter, Route } from 'react-router-dom';
 // Component
@@ -11,12 +11,25 @@ const mockStore = createStore(reducer);
 
 describe('CohortList', () => {
 	it('renders correctly', () => {
-		const tree = renderer.create(
-			<Provider store={mockStore}>
-				<BrowserRouter>
-					<Route to='/' component={CohortList} />
-				</BrowserRouter>
-			</Provider>).toJSON();
+		const tree = renderer
+			.create(
+				<Provider store={mockStore}>
+					<BrowserRouter>
+						<Route to="/" component={CohortList} />
+					</BrowserRouter>
+				</Provider>
+			)
+			.toJSON();
 		expect(tree).toMatchSnapshot();
 	});
+
+	it('should have initial state', () => {});
+
+	it('should render an Add a new class btn when state is empty', () => {});
+
+	it('should render rocket cards when stateful', () => {});
+
+	it('calls componentDidMount', () => {});
+
+	it('calls fetchData to update state', () => {});
 });
