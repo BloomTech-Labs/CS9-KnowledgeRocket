ff6b17bb9077aa903cc11d312bca445f69777b3e
Moises Dobarganes <designerexpert@gmail.com>
2018-08-21 12:32:11 -0400

    Fixed testing to mock store and Browser Router

--- client/src/tests/CohortList.test.js
+++ client/src/tests/CohortList.test.js
@@ -1,11 +1,24 @@
-// import React from 'react';
-// import renderer from 'react-test-renderer';
-// // Component
-// import CohortList from '../components/CohortList/CohortList';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import reducer from '../reducers';
+import { BrowserRouter, Route } from 'react-router-dom';
+// Component
+import CohortList from '../components/CohortList/CohortList';
 
-// describe('CohortList', () => {
-// 	it('renders correctly', () => {
-// 		const tree = renderer.create(<CohortList />).toJSON();
-// 		expect(tree).toMatchSnapshot();
-// 	});
-// });
+const mockState = {}
+
+const mockStore = createStore(reducer);
+
+describe('CohortList', () => {
+	it('renders correctly', () => {
+		const tree = renderer.create(
+			<Provider store={mockStore}>
+				<BrowserRouter>
+					<Route to='/' component={CohortList} />
+				</BrowserRouter>
+			</Provider>).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+});
