6baba8c6f849b9885908228945e48cc72cbf625a
JustinSHong <jhong2009@gmail.com>
2018-08-21 16:10:04 -0700

    styled CohortSettingForm

--- client/src/components/CohortSettingForm/CohortSettingForm.js
+++ client/src/components/CohortSettingForm/CohortSettingForm.js
@@ -4,16 +4,23 @@ import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 
-const CohortSettingForm = styled(Paper)`
-	border: 1px solid red;
-	width: 1000px;
-	height: 200px;
-`;
+// const CohortSettingForm = styled(Paper)`
+// 	border: 1px solid red;
+// 	width: 1000px;
+// 	height: 200px;
+// `;
 
-<Paper>
-	<Card>
-		<h1>DDDDD</h1>
-	</Card>
-</Paper>;
+// <Paper>
+// 	<Card>
+// 		<h1>DDDDD</h1>
+// 	</Card>
+// </Paper>;
+
+// CONTAINS SETTINGS: CLASS NAME, CC CHECKBOX, IMPORT CSV
+class CohortSettingForm extends Component {
+	render() {
+		return <h1 className={this.props.className}>Settings</h1>;
+	}
+}
 
 export default CohortSettingForm;
