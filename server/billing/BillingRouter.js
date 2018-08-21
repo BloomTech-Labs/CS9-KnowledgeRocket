9b5ea5cf489799376258e5a9c3b2ea882b6845aa
unknown <bsa6.23.94@gmail.com>
2018-08-21 13:19:51 -0400

    made a billing router and added the charge code to it

---
+++ server/billing/BillingRouter.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+
+router.route('/:type').post(post);
+
+async function post(req, res) {
+    const { type } = req.params;
+    const cost = type === 'monthly' ? 999 : 2999;
+    try {
+        let { status } = await stripe.charges.create({
+            amount: cost,
+            currency: 'usd',
+            description: `Subscription Charge for: ${type}`,
+            source: req.body.token,
+        });
+        //In here modify users to switch between pro and free
+        if (status) {
+            User.findById(req.body.id).then(currentUser => {
+                let expirationDate = Date.parse(currentUser.expiration);
+                let year =
+                    expirationDate > Date.now()
+                        ? expirationDate + 365 * 24 * 60 * 60 * 1000
+                        : Date.now() + 365 * 24 * 60 * 60 * 1000;
+                let month =
+                    expirationDate > Date.now()
+                        ? expirationDate + 30 * 24 * 60 * 60 * 1000
+                        : Date.now() + 30 * 24 * 60 * 60 * 1000;
+                console.log(year, month);
+                const newExpiration = type === 'monthly' ? month : year;
+                User.findByIdAndUpdate(req.body.id, {
+                    account: type,
+                    expiration: newExpiration,
+                })
+                    .then(found => {
+                        console.log(found);
+                        res.status(201).json({ status });
+                    })
+                    .catch(err => {
+                        res.status(500).json(err);
+                    });
+            });
+        } else {
+            res.status(500).json({ status });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+module.exports = router;
