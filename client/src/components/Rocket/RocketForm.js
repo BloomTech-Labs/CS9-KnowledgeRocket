951f3628cbedd888df7439d73ace3e068887a219
axhon <axhonruiz@gmail.com>
2018-08-22 12:40:19 -1000

    successful compile

--- client/src/components/Rocket/RocketForm.js
+++ client/src/components/Rocket/RocketForm.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import { withFormik } from 'formik';
-import { object, string } from 'yup';
+import { object } from 'yup';
 import './RocketForm.css';
 
 const rocketBlurb =
     'Knowledge rockets are short paragraphs followed by a multiple choice question. These are automatically sent two days, two weeks, and two months after a lesson is taught and are intended to cause the student to recall what they have learned. The rockets should not be used to provide exhaustive review. Instead, they ask the student to "reload" the mental maps that acquired during the lesson.';
 
 const RocketFormBase = props => {
-    const {
-        values,
-        errors,
-        dirty,
-        touched,
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        setFieldValue,
-        isSubmitting,
-    } = props;
+    const { values, handleSubmit, handleChange, handleBlur } = props;
 
     return (
         <form onSubmit={handleSubmit}>
@@ -36,9 +26,9 @@ const RocketFormBase = props => {
 
             {/* ROCKET QUESTION SECTION 1*/}
             <div className="RocketForm_wrapper">
-                <label htmlFor="explanation">{'Two Days - Review Text'}</label>
+                <label htmlFor="td.explanation">{'Two Days - Review Text'}</label>
                 <textarea
-                    name="explanation"
+                    name="td.explanation"
                     value={values.td.explanation}
                     placeholder="Two Day Review Text"
                     onChange={handleChange}
@@ -56,50 +46,51 @@ const RocketFormBase = props => {
                 />
             </div>
             <div className="RocketForm_questions_section">
-                <div className="RocketForm_question_wrapper">
-                    <input type="radio" />
-                    <label htmlFor="title" />
-                    <input
-                        name="title"
-                        value={values.td.title}
-                        placeholder="Answer 1"
-                        onChange={handleChange}
-                        className="questionField"
-                    />
-                </div>
-                <div className="RocketForm_question_wrapper">
-                    <input type="radio" />
-                    <label htmlFor="title" />
-                    <input
-                        name="title"
-                        value={values.td.title}
-                        placeholder="Answer 2"
-                        onChange={handleChange}
-                        className="questionField"
-                    />
-                </div>
-                <div className="RocketForm_question_wrapper">
-                    <input type="radio" />
-                    <label htmlFor="title" />
-                    <input
-                        name="Answer 2"
-                        value={values.td.title}
-                        placeholder="Answer 3"
-                        onChange={handleChange}
-                        className="questionField"
-                    />
-                </div>
-                <div className="RocketForm_question_wrapper">
-                    <input type="radio" />
-                    <label htmlFor="title" />
-                    <input
-                        name="title"
-                        value={values.td.title}
-                        placeholder="Answer 4"
-                        onChange={handleChange}
-                        className="questionField"
-                    />
-                </div>
+                <fieldset>
+                    <label>
+                        <input
+                            name="td.correct"
+                            checked={values.td.choices[0].correct}
+                            type="radio"
+                            onChange={handleChange}
+                        />
+                        Choice 1
+                    </label>
+                    <label>
+                        <input
+                            name="td.correct"
+                            checked={values.td.choices[1].correct}
+                            type="radio"
+                            onChange={handleChange}
+                        />
+                        Choice 2
+                    </label>
+                    <label>
+                        <input
+                            name="td.correct"
+                            checked={values.td.choices[2].correct}
+                            type="radio"
+                            onChange={handleChange}
+                        />
+                        Choice 3
+                    </label>
+                    <label>
+                        <input
+                            name="td.correct"
+                            checked={values.td.choices[3].text === values.td.correct}
+                            value={values.td.choices[3].text}
+                            type="radio"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        <input
+                            name="td.choices[3].text"
+                            type="text"
+                            onChange={handleChange}
+                            value={values.td.choices[3].text}
+                        />
+                    </label>
+                </fieldset>
             </div>
             {/* ROCKET QUESTION SECTION 2*/}
             <div className="RocketForm_wrapper">
@@ -235,54 +226,8 @@ const RocketFormBase = props => {
                     />
                 </div>
             </div>
-            <fieldset>
-                <label>
-                    <input
-                        name="td.correct"
-                        checked={values.td.choices[0].correct}
-                        type="radio"
-                        onChange={handleChange}
-                    />
-                    Choice 1
-                </label>
-                <label>
-                    <input
-                        name="td.correct"
-                        checked={values.td.choices[1].correct}
-                        type="radio"
-                        onChange={handleChange}
-                    />
-                    Choice 2
-                </label>
-                <label>
-                    <input
-                        name="td.correct"
-                        checked={values.td.choices[2].correct}
-                        type="radio"
-                        onChange={handleChange}
-                    />
-                    Choice 3
-                </label>
-                <label>
-                    <input
-                        name="td.correct"
-                        checked={values.td.choices[3].text === values.td.correct}
-                        value={values.td.choices[3].text}
-                        type="radio"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                    />
-                    <input
-                        name="td.choices[3].text"
-                        type="text"
-                        onChange={handleChange}
-                        value={values.td.choices[3].text}
-                    />
-                </label>
-            </fieldset>
-            <button type="submit" disabled={isSubmitting}>
-                submit
-            </button>
+
+            <button type="submit">submit</button>
         </form>
     );
 };
