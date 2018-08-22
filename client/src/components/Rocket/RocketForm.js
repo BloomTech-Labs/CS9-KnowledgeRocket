0fa170ae692a0b4fd9ecacdc237805c8b19ff671
axhon <axhonruiz@gmail.com>
2018-08-22 12:56:48 -1000

    begin styled components

--- client/src/components/Rocket/RocketForm.js
+++ client/src/components/Rocket/RocketForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Styled from 'styled-components';
 import { withFormik } from 'formik';
 import { object } from 'yup';
 import './RocketForm.css';
@@ -6,12 +7,26 @@ import './RocketForm.css';
 const rocketBlurb =
     'Knowledge rockets are short paragraphs followed by a multiple choice question. These are automatically sent two days, two weeks, and two months after a lesson is taught and are intended to cause the student to recall what they have learned. The rockets should not be used to provide exhaustive review. Instead, they ask the student to "reload" the mental maps that acquired during the lesson.';
 
+const FormGroup = Styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+`;
+
+const Blurb = Styled.p`
+    margin: 1rem 0;
+    text-align: justify;
+`;
+
+const TextArea = Blurb.extend``.withComponent('textarea');
+
 const RocketFormBase = props => {
     const { values, handleSubmit, handleChange, handleBlur } = props;
 
     return (
         <form onSubmit={handleSubmit}>
-            <div className="RocketForm_wrapper">
+            <FormGroup>
                 <label htmlFor="title" />
                 <input
                     name="title"
@@ -19,100 +34,103 @@ const RocketFormBase = props => {
                     placeholder="Title"
                     onChange={handleChange}
                 />
-            </div>
-            <div className="RocketForm_wrapper">
-                <p>{rocketBlurb}</p>
-            </div>
+            </FormGroup>
+            <FormGroup>
+                <Blurb>{rocketBlurb}</Blurb>
+            </FormGroup>
 
             {/* ROCKET QUESTION SECTION 1*/}
-            <div className="RocketForm_wrapper">
-                <label htmlFor="td.explanation">{'Two Days - Review Text'}</label>
-                <textarea
-                    name="td.explanation"
-                    value={values.td.explanation}
-                    placeholder="Two Day Review Text"
-                    onChange={handleChange}
-                    style={{ width: '100%', height: '4rem' }}
-                />
-            </div>
-            <div className="RocketForm_wrapper">
-                <label htmlFor="td.question">{'Two Days - Review Question'}</label>
-                <textarea
-                    name="td.question"
-                    value={values.td.question}
-                    placeholder="Two Day Review Text"
-                    onChange={handleChange}
-                    style={{ width: '100%', height: '4rem' }}
-                />
-            </div>
-            <div className="RocketForm_questions_section">
-                <fieldset>
-                    <label>
-                        <input
-                            name="td.correct"
-                            checked={values.td.choices[0].correct}
-                            type="radio"
-                            onChange={handleChange}
-                        />
-                        Choice 1
-                    </label>
-                    <label>
-                        <input
-                            name="td.correct"
-                            checked={values.td.choices[1].correct}
-                            type="radio"
-                            onChange={handleChange}
-                        />
-                        Choice 2
-                    </label>
-                    <label>
-                        <input
-                            name="td.correct"
-                            checked={values.td.choices[2].correct}
-                            type="radio"
-                            onChange={handleChange}
-                        />
-                        Choice 3
-                    </label>
-                    <label>
-                        <input
-                            name="td.correct"
-                            checked={values.td.choices[3].text === values.td.correct}
-                            value={values.td.choices[3].text}
-                            type="radio"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        <input
-                            name="td.choices[3].text"
-                            type="text"
-                            onChange={handleChange}
-                            value={values.td.choices[3].text}
-                        />
-                    </label>
-                </fieldset>
-            </div>
+            <section>
+                <FormGroup>
+                    <label htmlFor="td.explanation">{'Two Days - Review Text'}</label>
+                    <TextArea
+                        name="td.explanation"
+                        value={values.td.explanation}
+                        placeholder="Two Day Review Text"
+                        onChange={handleChange}
+                        style={{ width: '100%', height: '4rem' }}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <label htmlFor="td.question">{'Two Days - Review Question'}</label>
+                    <TextArea
+                        name="td.question"
+                        value={values.td.question}
+                        placeholder="Two Day Review Text"
+                        onChange={handleChange}
+                        style={{ width: '100%', height: '4rem' }}
+                    />
+                </FormGroup>
+                <div className="RocketForm_questions_section">
+                    <fieldset>
+                        <label>
+                            <input
+                                name="td.correct"
+                                checked={values.td.choices[0].correct}
+                                type="radio"
+                                onChange={handleChange}
+                            />
+                            Choice 1
+                        </label>
+                        <label>
+                            <input
+                                name="td.correct"
+                                checked={values.td.choices[1].correct}
+                                type="radio"
+                                onChange={handleChange}
+                            />
+                            Choice 2
+                        </label>
+                        <label>
+                            <input
+                                name="td.correct"
+                                checked={values.td.choices[2].correct}
+                                type="radio"
+                                onChange={handleChange}
+                            />
+                            Choice 3
+                        </label>
+                        <label>
+                            <input
+                                name="td.correct"
+                                checked={values.td.choices[3].text === values.td.correct}
+                                value={values.td.choices[3].text}
+                                type="radio"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <input
+                                name="td.choices[3].text"
+                                type="text"
+                                onChange={handleChange}
+                                value={values.td.choices[3].text}
+                            />
+                        </label>
+                    </fieldset>
+                </div>
+            </section>
+
             {/* ROCKET QUESTION SECTION 2*/}
-            <div className="RocketForm_wrapper">
+            <FormGroup>
                 <label htmlFor="title">{'Two Weeks - Review Text'}</label>
-                <textarea
+                <TextArea
                     name="title"
                     value={values.td.title}
                     placeholder="Two Weeks Review Text"
                     onChange={handleChange}
                     style={{ width: '100%', height: '4rem' }}
                 />
-            </div>
-            <div className="RocketForm_wrapper">
+            </FormGroup>
+            <FormGroup>
                 <label htmlFor="title">{'Two Weeks - Review Question'}</label>
-                <textarea
+                <TextArea
                     name="title"
                     value={values.td.title}
                     placeholder="Two Weeks Review Text"
                     onChange={handleChange}
                     style={{ width: '100%', height: '4rem' }}
                 />
-            </div>
+            </FormGroup>
             <div className="RocketForm_questions_section">
                 <div className="RocketForm_question_wrapper">
                     <input type="radio" />
@@ -160,26 +178,26 @@ const RocketFormBase = props => {
                 </div>
             </div>
             {/* ROCKET QUESTION SECTION 3*/}
-            <div className="RocketForm_wrapper">
+            <FormGroup>
                 <label htmlFor="title">{'Two Months - Review Text'}</label>
-                <textarea
+                <TextArea
                     name="title"
                     value={values.td.title}
                     placeholder="Two Months Review Text"
                     onChange={handleChange}
                     style={{ width: '100%', height: '4rem' }}
                 />
-            </div>
-            <div className="RocketForm_wrapper">
+            </FormGroup>
+            <FormGroup>
                 <label htmlFor="title">{'Two Months - Review Question'}</label>
-                <textarea
+                <TextArea
                     name="title"
                     value={values.td.title}
                     placeholder="Two Months Review Text"
                     onChange={handleChange}
                     style={{ width: '100%', height: '4rem' }}
                 />
-            </div>
+            </FormGroup>
             <div className="RocketForm_questions_section">
                 <div className="RocketForm_question_wrapper">
                     <input type="radio" />
