096adad2259d6d5b76453b2576729c36499a3276
JustinSHong <jhong2009@gmail.com>
2018-08-21 17:09:06 -0700

    added first name and email fields

---
+++ client/src/components/CohortAddStudentsForm/CohortAddStudentsForm.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+// Material Components
+import Card from '@material-ui/core/Card';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+
+const StylizedInput = styled(Input)`
+	padding: 5px;
+`;
+
+// CONTAINS THE LAST NAME, FIRST NAME, EMAIL, ADD BTN TO ADD STUDENTS
+class CohortAddStudentsForm extends Component {
+	render() {
+		return (
+			<Card className={this.props.className}>
+				<StylizedInput defaultValue="Last Name" disableUnderline="true" />
+				<StylizedInput defaultValue="First Name" disableUnderline="true" />
+				<StylizedInput defaultValue="Email" disableUnderline="true" />
+			</Card>
+		);
+	}
+}
+
+export default CohortAddStudentsForm;
