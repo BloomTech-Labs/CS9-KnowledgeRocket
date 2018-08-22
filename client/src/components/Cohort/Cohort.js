d34c30a7327fb174a5f421bc4e9508f6c039cbf6
JustinSHong <jhong2009@gmail.com>
2018-08-22 15:12:16 -0700

    removed unneeded import

--- client/src/components/Cohort/Cohort.js
+++ client/src/components/Cohort/Cohort.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 // Material Components
 import Card from '@material-ui/core/Card';
