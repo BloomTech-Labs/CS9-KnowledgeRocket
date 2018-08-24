8ba3d1d9917ba6246e640832094b4c757b1965cc
axhon <axhonruiz@gmail.com>
2018-08-24 02:25:16 -1000

    rudimentary validation done

--- client/src/components/Rocket/RocketForm.js
+++ client/src/components/Rocket/RocketForm.js
@@ -1,51 +1,63 @@
 import React from 'react';
 import { withFormik } from 'formik';
-import { object } from 'yup';
-import { FormGroup, Blurb, QuestionChoices } from './FormGroup';
+import { object, string, array } from 'yup';
+import { FormGroup, Blurb, QuestionChoices, ErrorText, errorHelper } from './FormGroup';
 
 const rocketBlurb =
     'Knowledge rockets are short paragraphs followed by a multiple choice question. These are automatically sent two days, two weeks, and two months after a lesson is taught and are intended to cause the student to recall what they have learned. The rockets should not be used to provide exhaustive review. Instead, they ask the student to "reload" the mental maps that acquired during the lesson.';
 
-const RocketFormBase = ({ values, handleSubmit, handleChange, handleBlur, errors }) => (
-    <form onSubmit={handleSubmit}>
-        <FormGroup>
-            <label htmlFor="title" />
-            <input name="title" value={values.title} placeholder="Title" onChange={handleChange} />
-        </FormGroup>
-        <FormGroup>
-            <Blurb>{rocketBlurb}</Blurb>
-        </FormGroup>
+const RocketFormBase = ({ values, handleSubmit, handleChange, handleBlur, errors, touched }) => {
+    const displayError = errorHelper(errors, touched);
+    return (
+        <form onSubmit={handleSubmit}>
+            <FormGroup>
+                <label htmlFor="title" />
+                <input
+                    name="title"
+                    value={values.title}
+                    placeholder="Title"
+                    onChange={handleChange}
+                />
+                <ErrorText>{displayError('title')}</ErrorText>
+            </FormGroup>
+            <FormGroup>
+                <Blurb>{rocketBlurb}</Blurb>
+            </FormGroup>
 
-        {/* ROCKET QUESTION SECTION 1*/}
-        <QuestionChoices
-            values={values}
-            handleBlur={handleBlur}
-            handleChange={handleChange}
-            increment="td"
-            errors={errors.td}
-        />
+            {/* ROCKET QUESTION SECTION 1*/}
+            <QuestionChoices
+                values={values}
+                handleBlur={handleBlur}
+                handleChange={handleChange}
+                increment="td"
+                errors={errors.td}
+                touched={touched.td}
+            />
 
-        {/* ROCKET QUESTION SECTION 2*/}
-        <QuestionChoices
-            values={values}
-            handleBlur={handleBlur}
-            handleChange={handleChange}
-            increment="tw"
-            errors={errors.tw}
-        />
+            {/* ROCKET QUESTION SECTION 2*/}
+            <QuestionChoices
+                values={values}
+                handleBlur={handleBlur}
+                handleChange={handleChange}
+                increment="tw"
+                errors={errors.tw}
+                touched={touched.tw}
+            />
 
-        {/* ROCKET QUESTION SECTION 3*/}
-        <QuestionChoices
-            values={values}
-            handleBlur={handleBlur}
-            handleChange={handleChange}
-            increment="tm"
-            errors={errors.tm}
-        />
+            {/* ROCKET QUESTION SECTION 3*/}
+            <QuestionChoices
+                values={values}
+                handleBlur={handleBlur}
+                handleChange={handleChange}
+                increment="tm"
+                errors={errors.tm}
+                touched={touched.tm}
+            />
 
-        <button type="submit">submit</button>
-    </form>
-);
+            <button type="submit">submit</button>
+        </form>
+    );
+};
 
 const defaultProps = {
     td: generateDefaults(),
@@ -64,11 +76,38 @@ const RocketForm = withFormik({
         Object.assign(defaultProps, { ...props });
         return defaultProps;
     },
-    validationSchema: object().shape({}),
+    validationSchema: object().shape({
+        title: string()
+            .trim()
+            .required('Title is required.'),
+        td: rocketShape(),
+        tw: rocketShape(),
+        tm: rocketShape(),
+    }),
     displayName: 'RocketForm',
 })(RocketFormBase);
 
 export default RocketForm;
+
+function rocketShape() {
+    return object().shape({
+        explanation: string()
+            .trim()
+            .required('Review Text is required.'),
+        question: string()
+            .trim()
+            .required('You must include a question.'),
+        correct: string()
+            .trim()
+            .required('You must choose a correct answer.'),
+        choices: array().of(
+            string()
+                .trim()
+                .min(1)
+                .required()
+        ),
+    });
+}
 
 function generateDefaults() {
     return {
