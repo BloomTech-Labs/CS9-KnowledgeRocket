0b1955b915d5d01550bd0ea58c28427e21aa884b
JustinSHong <jhong2009@gmail.com>
2018-08-24 10:02:52 -0700

    pass student to StudentCard

--- client/src/components/CohortStudentList/CohortStudentList.js
+++ client/src/components/CohortStudentList/CohortStudentList.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 // Material Components
 import Card from '@material-ui/core/Card';
 // Components
 import CohortStudentCard from '../CohortStudentCard/CohortStudentCard';
+
+function mapStateToProps(state) {
+	return {
+		state,
+	};
+}
 
 // CONTAINS THE LAST NAME, FIRST NAME, EMAIL, ADD BTN TO ADD STUDENTS
 class CohortStudentList extends Component {
@@ -10,14 +17,10 @@ class CohortStudentList extends Component {
 		return (
 			<Card className={this.props.className}>
 				{/* Render all students added */}
-				{/*this.props.students.map((student, index) => <CohortStudentCard student={student} />)*/}
-
-				<CohortStudentCard />
-				<CohortStudentCard />
-				<CohortStudentCard />
+				{this.props.students.map((student, index) => <CohortStudentCard student={student} />)}
 			</Card>
 		);
 	}
 }
 
-export default CohortStudentList;
+export default connect(mapStateToProps)(CohortStudentList);
