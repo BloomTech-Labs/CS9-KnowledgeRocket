a00379864562f87da9fa126fd23bd9bdec4e4fec
Moises Dobarganes <designerexpert@gmail.com>
2018-08-27 14:44:14 -0400

    Implemented Modular Settings based on Auth Provider

--- client/src/components/Settings/Settings.js
+++ client/src/components/Settings/Settings.js
@@ -16,6 +16,8 @@ class Settings extends Component {
         currentPW: '',
         newPW: '',
         ccEmail: '',
+        authProvider: '',
+        
     };
     componentDidMount() {
         // Checks for User to be Authenticated
@@ -25,6 +27,7 @@ class Settings extends Component {
             this.props.history.push('/rocket/auth');
         }
         this.props.generateBreadCrumbs(this.props.history.location.pathname);
+        this.setState({authProvider: this.props.state.user.authProvider})
     }
 
     handleUpdateSettings = e => {
@@ -40,64 +43,91 @@ class Settings extends Component {
 
     render() {
         console.log(this.state);
-        return (
-            <div className="Main_container">
-                <form>
-                    <FormControl className="Settings_form">
-                        <header className="title10">User Account Settings</header>
-                        <FormLabel
-                            className={
-                                this.props.state.user.authProvider === 'email'
-                                    ? 'Settings_info'
-                                    : 'Settings_info--Oauth'
-                            }
-                        >
-                            {this.props.state.user.authProvider === 'email'
-                                ? 'Update your account information below.'
-                                : `You must update your account information with: ${
-                                      this.props.state.user.authProvider
-                                  }`}
-                        </FormLabel>
-                        <TextField
-                            id="currentPW"
-                            label="Current Password"
-                            className="Settings_input"
-                            type="password"
-                            margin="normal"
-                            onChange={this.handleInput}
-                            value={this.state.currentPW}
-                        />
-                        <TextField
-                            id="newPW"
-                            label="New Password"
-                            className="Settings_input"
-                            type="password"
-                            margin="normal"
-                            onChange={this.handleInput}
-                            value={this.state.newPW}
-                        />
-                        <TextField
-                            id="ccEmail"
-                            label="Change CC Email?"
-                            className="Settings_input"
-                            type="email"
-                            margin="normal"
-                            onChange={this.handleInput}
-                        />
-                        <Button
-                            onClick={this.handleUpdateSettings}
-                            type="button"
-                            className="Settings_submit"
-                            variant="contained"
-                            color="primary"
-                            value={this.state.ccEmail}
-                        >
-                            Submit
-                        </Button>
-                    </FormControl>
-                </form>
-            </div>
-        );
+        if (this.state.authProvider === 'email') {
+            return (
+                <div className="Main_container">
+                    <form>
+                        <FormControl className="Settings_form">
+                            <header className="title10">User Account Settings</header>
+                            <FormLabel
+                                className={'Settings_info'}
+                            >
+                                {'Update your account information below.'}
+                            </FormLabel>
+                            <TextField
+                                id="currentPW"
+                                label="Current Password"
+                                className="Settings_input"
+                                type="password"
+                                margin="normal"
+                                onChange={this.handleInput}
+                                value={this.state.currentPW}
+                            />
+                            <TextField
+                                id="newPW"
+                                label="New Password"
+                                className="Settings_input"
+                                type="password"
+                                margin="normal"
+                                onChange={this.handleInput}
+                                value={this.state.newPW}
+                            />
+                            <TextField
+                                id="ccEmail"
+                                label="Change CC Email?"
+                                className="Settings_input"
+                                type="email"
+                                margin="normal"
+                                onChange={this.handleInput}
+                            />
+                            <Button
+                                onClick={this.handleUpdateSettings}
+                                type="button"
+                                className="Settings_submit"
+                                variant="contained"
+                                color="primary"
+                                value={this.state.ccEmail}
+                            >
+                                Submit
+                            </Button>
+                        </FormControl>
+                    </form>
+                </div>
+            );
+        } else {
+            return (
+                <div className="Main_container">
+                    <form>
+                        <FormControl className="Settings_form">
+                            <header className="title10">User Account Settings</header>
+                            <FormLabel
+                                className={'Settings_info--Oauth'}
+                            >
+                                {`You must update your account information with: ${this.state.authProvider}`}
+                            </FormLabel>
+                            <TextField
+                                id="ccEmail"
+                                label="Change CC Email?"
+                                className="Settings_input"
+                                type="email"
+                                margin="normal"
+                                onChange={this.handleInput}
+                            />
+                            <Button
+                                onClick={this.handleUpdateSettings}
+                                type="button"
+                                className="Settings_submit"
+                                variant="contained"
+                                color="primary"
+                                value={this.state.ccEmail}
+                            >
+                                Submit
+                            </Button>
+                        </FormControl>
+                    </form>
+                </div>
+            );
+        }
     }
 }
 
