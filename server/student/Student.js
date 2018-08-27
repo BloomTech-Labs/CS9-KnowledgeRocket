b9e1f171295debc725b79b0e0d418a585d5ce088
JustinSHong <jhong2009@gmail.com>
2018-08-26 18:50:35 -0700

    set up remove pre hook to remove students from students field in Cohort

--- server/student/Student.js
+++ server/student/Student.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
+const Cohort = require('../cohort/Cohort');
+const User = require('../user/User');
 
 const Student = mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
 });
 
-// Student.pre('remove', function(next) {
-// 	// Remove all the assignment docs that reference the removed person.
-// 	this.model('Cohort').remove({ students: this._id }, next);
-// });
-
-// Student.pre('update', function(next) {
-// 	this.model('User').update({}, { $pull: { students: this._id } }, { multi: true }, next);
-// });
+// find students inside a cohort and remove them from the students field
+Student.pre('remove', function(next) {
+	Cohort.update({ students: this._id }, { $pull: { students: this._id } }, { multi: true }).exec();
+	next();
+});
 
 module.exports = mongoose.model('Students', Student);
