15c734407b51b46459f5c7bd9a995339c3ff7a55
JustinSHong <jhong2009@gmail.com>
2018-08-26 19:02:50 -0700

    set up styled components

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { Button, SvgIcon } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { logOutUser } from '../../actions';
-import './index.css';
+// import './index.css';
 
 function mapStateToProps(state) {
     return {
         state,
     };
 }
+
+const StyledNavBarContainer = styled.div`
+    padding: 1rem;
+    display: flex;
+    width: 100%;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const StyledBreadCrumbContainer = styled.div`
+    display: flex;
+`;
+
+const StyledCrumb = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0.1rem;
+`;
 
 class NavBar extends Component {
     componentDidMount() {
@@ -26,30 +48,30 @@ class NavBar extends Component {
     render() {
         console.log(this.props);
         return (
-            <div className="NavBar_container">
-                <div className="NavBar_breadcrumbs">
+            <StyledNavBarContainer>
+                <StyledBreadCrumbContainer>
                     {this.props.state.breadcrumb.labels.map((item, index) => {
                         return (
-                            <div className="NavBar_breadcrumb" key={item + index}>
-                                <Link to={this.props.state.breadcrumb.paths[index]} style={{textDecoration: 'none'}}>
+                            <StyledCrumb key={item + index}>
+                                <Link
+                                    to={this.props.state.breadcrumb.paths[index]}
+                                    style={{ textDecoration: 'none' }}
+                                >
                                     <Button> {item} </Button>
                                 </Link>
                                 <SvgIcon color="primary">
                                     <path d="M5.59,7.41L7,6L13,12L7,18L5.59,16.59L10.17,12L5.59,7.41M11.59,7.41L13,6L19,12L13,18L11.59,16.59L16.17,12L11.59,7.41Z" />
                                 </SvgIcon>
-                            </div>
+                            </StyledCrumb>
                         );
                     })}
-                </div>
+                </StyledBreadCrumbContainer>
                 <Button onClick={this.handleLogOut} variant="contained" color="secondary">
                     Sign-Out
                 </Button>
-            </div>
+            </StyledNavBarContainer>
         );
     }
 }
 
-export default connect(
-    mapStateToProps,
-    { logOutUser }
-)(NavBar);
+export default connect(mapStateToProps, { logOutUser })(NavBar);
