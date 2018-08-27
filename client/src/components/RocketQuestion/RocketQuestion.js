3ebe2fcb59ce1df56051e2537fbd56387b7dbd79
unknown <bsa6.23.94@gmail.com>
2018-08-27 15:52:40 -0400

    working on rocket question basic layout is shaping up

--- client/src/components/RocketQuestion/RocketQuestion.js
+++ client/src/components/RocketQuestion/RocketQuestion.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+import FormLabel from '@material-ui/core/FormLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
 
 const url = process.env.REACT_APP_SERVER;
 
@@ -21,12 +26,29 @@ const defaultState = {
     },
 };
 
+const QuestionHeader = styled.div`
+    margin-left: 2rem;
+    margin-top: 2rem;
+`;
+const QuestionText = styled.p`
+    font-size: 1.3rem;
+    margin-bottom: 1.5rem;
+`;
+
+const StyledHeaders = styled.h1`
+    align-self: flex-start;
+    font-size 2rem;
+    margin-bottom: 1rem;
+    font-weight: 460;
+    font-family: 'Roboto', serif;
+`;
 class RocketQuestion extends Component {
-    state = defaultState;
+    state = { ...defaultState };
 
     componentDidMount() {
         const questionID = this.props.match.params.question;
         // Get stuff about the question from the server
+        console.log(this.props);
         axios
             .get(`${url}/api/question/${questionID}`)
             .then(response => {
@@ -42,7 +64,6 @@ class RocketQuestion extends Component {
     }
 
     handleSubmit = e => {};
-
     handleInput = e => {
         this.setState({ [e.target.id]: e.target.value });
     };
@@ -50,21 +71,36 @@ class RocketQuestion extends Component {
     render() {
         console.log('My state is:', this.state);
         return (
-            <div className="Question_container">
-                <div>
-                    <h1>{this.state.rocketQuestion.title}</h1>
-                    <p>{this.state.rocketQuestion.explanation}</p>
+            <QuestionHeader className="Question_container">
+                <div className="Question_text">
+                    <StyledHeaders>{this.state.rocketQuestion.title}</StyledHeaders>
+                    <QuestionText>{this.state.rocketQuestion.explanation}</QuestionText>
                 </div>
-                <div>
-                    <h2>{'Question:'}</h2>
-                    <p>{this.state.rocketQuestion.question}</p>
+                <div className="Question_question">
+                    <StyledHeaders>{'Question:'}</StyledHeaders>
+                    <QuestionText>{this.state.rocketQuestion.question}</QuestionText>
                 </div>
-                <div>
-                    {this.state.rocketQuestion.choices.map((answer, index) => {
-                        return <div key={`${index}`}>[ ] {answer.text}</div>;
-                    })}
+                <div className="Question_answers">
+                    <FormLabel component="legend" className="legend">
+                        Please Answer The Question:
+                    </FormLabel>
+                    <RadioGroup value={this.state.value} id="value" onChange={this.handleInput}>
+                        {this.state.rocketQuestion.choices.map((answer, index) => {
+                            return (
+                                <div key={`${index}`}>
+                                    <FormControlLabel
+                                        id={index}
+                                        value={answer.text}
+                                        control={<Radio color="primary" />}
+                                        label={answer.text}
+                                        labelPlacement="end"
+                                    />
+                                </div>
+                            );
+                        })}
+                    </RadioGroup>
                 </div>
-            </div>
+            </QuestionHeader>
         );
     }
 }
