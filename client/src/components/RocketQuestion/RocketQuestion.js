287aa60ebf81c3c3ae7d1bd6dce1958f482dd645
unknown <bsa6.23.94@gmail.com>
2018-08-27 13:20:25 -0400

    working on rocket question layout

--- client/src/components/RocketQuestion/RocketQuestion.js
+++ client/src/components/RocketQuestion/RocketQuestion.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 
 class RocketQuestion extends Component {
+    state = {};
     render() {
-        return (
-            <div>
-                
-            </div>
-        );
+        return <div className="Main_container" />;
     }
 }
 
