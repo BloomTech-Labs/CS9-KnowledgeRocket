f1490e758ebe4448b0e36bfeaa532336a42ac982
axhon <axhonruiz@gmail.com>
2018-08-27 11:57:43 -1000

    chore: prettify

--- client/src/tests/RocketForm.test.js
+++ client/src/tests/RocketForm.test.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import { renderSnapshotTest } from './utils';
 import 'jest-styled-components';
 // Component
 import RForm from '../components/Rocket/RocketForm';
 
-describe('RocketForm', () => {
-    it('renders correctly', () => {
-        const tree = renderer.create(<RForm />).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+describe('<RocketForm />', () => {
+    renderSnapshotTest(RForm);
 });
