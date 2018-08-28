a2fd9c28eb466e6a6623062723995cd9cdc8e97f
BigBricks <36277515+BigBricks@users.noreply.github.com>
2018-08-28 15:27:06 -0400

    pascal case styledcontent container

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -15,7 +15,7 @@ import Settings from '../Settings/Settings';
 import RocketView from '../Rocket/RocketView';
 import RocketQuestion from '../RocketQuestion/RocketQuestion';
 
-const StyledContent_container = styled.div`
+const StyledContent_Container = styled.div`
     background: #e0f2f1;
     display: flex;
     flex-direction: row;
@@ -44,7 +44,7 @@ class Routes extends Component {
                         <Route path="/rocket/classes" exact component={CohortList} />
                         <Route path="/rocket/settings" exact component={Settings} />
                         <Route path="/rocket/auth" exact component={Auth} />
-                    </StyledContent_container>
+                    </StyledContent_Container>
                     <Route path="/" exact component={Home} />
                     <Route path="/question/:question/:student" exact component={RocketQuestion} />
                 </div>
