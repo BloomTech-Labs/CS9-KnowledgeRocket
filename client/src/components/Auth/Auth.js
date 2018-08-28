a4ae3d8d5bab3e0c85a17a3cdae82d662ec29092
JustinSHong <jhong2009@gmail.com>
2018-08-28 11:32:35 -0700

    removed wrapper around form elements

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -92,85 +92,83 @@ class Auth extends Component {
         // console.log('state user', this.state.authenticated);
         return (
             <div className="Main_container">
-                <LoginPortal>
-                    <h1 className="Auth_header">Please Sign-in or Sign-up.</h1>
-                    <div className="flex-column-centered">
-                        <Input
-                            className="Auth_input"
-                            type="email"
-                            name="email"
-                            autoFocus={true}
-                            onChange={this.handleInput}
-                            disableUnderline={true}
-                        />
-                        <Input
-                            className="Auth_input"
-                            type="password"
-                            name="password"
-                            onChange={this.handleInput}
-                            disableUnderline={true}
-                        />
+                <h1 className="Auth_header">Please Sign-in or Sign-up.</h1>
+                <div className="flex-column-centered">
+                    <Input
+                        className="Auth_input"
+                        type="email"
+                        name="email"
+                        autoFocus={true}
+                        onChange={this.handleInput}
+                        disableUnderline={true}
+                    />
+                    <Input
+                        className="Auth_input"
+                        type="password"
+                        name="password"
+                        onChange={this.handleInput}
+                        disableUnderline={true}
+                    />
+                </div>
+                <div className="flex-column-centered">
+                    <div className="flex-row-centered">
+                        <Button
+                            className="Auth_button"
+                            variant="contained"
+                            color="primary"
+                            onClick={this.handleSignUp}
+                        >
+                            Sign-Up
+                        </Button>
+                        <Button
+                            className="Auth_button"
+                            variant="contained"
+                            color="primary"
+                            onClick={this.handleSignIn}
+                        >
+                            Sign-In
+                        </Button>
                     </div>
-                    <div className="flex-column-centered">
-                        <div className="flex-row-centered">
-                            <Button
-                                className="Auth_button"
-                                variant="contained"
-                                color="primary"
-                                onClick={this.handleSignUp}
-                            >
-                                Sign-Up
-                            </Button>
-                            <Button
-                                className="Auth_button"
-                                variant="contained"
-                                color="primary"
-                                onClick={this.handleSignIn}
-                            >
-                                Sign-In
-                            </Button>
-                        </div>
-                        <div className="Auth_oauth_section">
-                            <Button
-                                className="Auth_button"
-                                variant="contained"
-                                color="primary"
-                                onClick={this.handleSignInGoogle}
-                            >
-                                Google Log In
-                            </Button>
-                            <Button
-                                className="Auth_button"
-                                variant="contained"
-                                color="primary"
-                                onClick={this.handleSignInFacebook}
-                            >
-                                Facebook Log In
-                            </Button>
-                            <Button
-                                className="Auth_button"
-                                variant="contained"
-                                color="primary"
-                                onClick={this.handleSignInTwitter}
-                            >
-                                Twitter Log In
-                            </Button>
-                        </div>
+                    <div className="Auth_oauth_section">
+                        <Button
+                            className="Auth_button"
+                            variant="contained"
+                            color="primary"
+                            onClick={this.handleSignInGoogle}
+                        >
+                            Google Log In
+                        </Button>
+                        <Button
+                            className="Auth_button"
+                            variant="contained"
+                            color="primary"
+                            onClick={this.handleSignInFacebook}
+                        >
+                            Facebook Log In
+                        </Button>
+                        <Button
+                            className="Auth_button"
+                            variant="contained"
+                            color="primary"
+                            onClick={this.handleSignInTwitter}
+                        >
+                            Twitter Log In
+                        </Button>
                     </div>
-                    <div className="flex-row-centered Auth_prompt-fail">
-                        {/* THIS SECTION WILL HANDLE USER AUTH ERROR MESSAGES */}
-                        {this.props.user.status === 'FAILED' ? (
-                            <p>
-                                Authentication failed. Check your email and password and try again.
-                                Thank you.
-                            </p>
-                        ) : this.props.user.status === 'ADDING_USER' ||
-                        this.props.user.status === 'LOGGING_IN_USER' ? (
-                            <Progress />
-                        ) : null}
-                    </div>
-                    {this.props.user.authenticated ? <Redirect to="/rocket" /> : null}
-                </LoginPortal>
+                </div>
+                <div className="flex-row-centered Auth_prompt-fail">
+                    {/* THIS SECTION WILL HANDLE USER AUTH ERROR MESSAGES */}
+                    {this.props.user.status === 'FAILED' ? (
+                        <p>
+                            Authentication failed. Check your email and password and try again.
+                            Thank you.
+                        </p>
+                    ) : this.props.user.status === 'ADDING_USER' ||
+                    this.props.user.status === 'LOGGING_IN_USER' ? (
+                        <Progress />
+                    ) : null}
+                </div>
+                {this.props.user.authenticated ? <Redirect to="/rocket" /> : null}
             </div>
         );
     }
