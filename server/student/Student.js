ba583e7fa6c86b171621eb20d29d05107f5b4001
axhon <axhonruiz@gmail.com>
2018-08-28 12:02:32 -1000

    bugfix: changed Students Model name to singular form

--- server/student/Student.js
+++ server/student/Student.js
@@ -4,16 +4,20 @@ const Cohort = require('../cohort/Cohort');
 const User = require('../user/User');
 
 const Student = mongoose.Schema({
-	firstName: { type: String, required: true },
-	lastName: { type: String, required: true },
-	email: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
 });
 
 // called when a delete request is made for a student
 // find students inside a cohort and remove them from the students field
 Student.pre('remove', function(next) {
-	Cohort.update({ students: this._id }, { $pull: { students: this._id } }, { multi: true }).exec();
-	next();
+    Cohort.update(
+        { students: this._id },
+        { $pull: { students: this._id } },
+        { multi: true }
+    ).exec();
+    next();
 });
 
-module.exports = mongoose.model('Students', Student);
+module.exports = mongoose.model('Student', Student);
