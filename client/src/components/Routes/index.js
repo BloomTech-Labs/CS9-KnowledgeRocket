b3c62fb38d799f5e47fee80a82935ffa4f6492f3
JustinSHong <jhong2009@gmail.com>
2018-08-28 11:45:36 -0700

    styled Content_container as styled component, set media query at 1055px

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import styled from 'styled-components';
 // Components
 import Home from '../Home/Home';
 import NavBar from '../NavBar/NavBar';
@@ -14,13 +15,26 @@ import Settings from '../Settings/Settings';
 import RocketView from '../Rocket/RocketView';
 import RocketQuestion from '../RocketQuestion/RocketQuestion';
 
+const StyledContent_container = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-left: 9rem;
+    height: 100vh;
+    padding: 0 1rem;
+
+    @media (max-width: 1055px) {
+        margin: 0 auto;
+        max-width: 100%;
+    }
+`;
+
 class Routes extends Component {
     render() {
         return (
             <BrowserRouter>
                 <div className="routeContainer">
                     <Route path="/rocket" component={NavBar} />
-                    <div className="Content_container">
+                    <StyledContent_container>
                         <Route path="/rocket" exact component={RocketList} />
                         <Route path="/rocket/new" exact component={Rocket} />
                         <Route path="/rocket/view/:id" exact component={RocketView} />
@@ -30,7 +44,7 @@ class Routes extends Component {
                         <Route path="/rocket/classes" exact component={CohortList} />
                         <Route path="/rocket/settings" exact component={Settings} />
                         <Route path="/rocket/auth" exact component={Auth} />
-                    </div>
+                    </StyledContent_container>
                     <Route path="/" exact component={Home} />
                     <Route path="/question/:question/:student" exact component={RocketQuestion} />
                 </div>
