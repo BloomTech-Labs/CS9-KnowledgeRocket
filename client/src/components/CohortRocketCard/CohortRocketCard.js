76f63e1490c5d724c56537356955d001e74350cb
Moises Dobarganes <designerexpert@gmail.com>
2018-08-28 19:27:04 -0400

    CohortRocketCard: Date formatting and Re-Scheduling Done

--- client/src/components/CohortRocketCard/CohortRocketCard.js
+++ client/src/components/CohortRocketCard/CohortRocketCard.js
@@ -8,7 +8,6 @@ import TextField from '@material-ui/core/TextField';
 import FormLabel from '@material-ui/core/FormLabel';
 import Button from '@material-ui/core/Button';
 import moment from 'moment-timezone';
-// import momentTimezone from 'moment-timezone';
 import {appendRocket} from '../../actions';
 
 function mapStateToProps(state) {
