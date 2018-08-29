5d364ef4e8bdd1d366061aeefe9e4e453e871164
axhon <axhonruiz@gmail.com>
2018-08-29 11:28:26 -1000

    documentation update,

--- server/mail/generateEmail.js
+++ server/mail/generateEmail.js
@@ -25,6 +25,12 @@ const validationSchema = yup.object().shape({
         .nullable(),
 });
 
+/**
+ *  Promise that resolves with an object containing whether or not the object
+ *  being validated isValid, and errors if there are any.
+ * @param {*} validatee Object that is being validated
+ * @param {typeof validationSchema} schema Validation object that does the actual validation.
+ */
 const Validator = async (validatee, schema = validationSchema) => {
     try {
         await schema.validate(validatee);
@@ -56,31 +62,42 @@ const __generateEmail = ({ to, from, replyTo, subject, text, html, cc = null }) 
         to,
         from,
         replyTo,
-        subject,
-        text,
-        html,
+        subject, // Knowledge Rocket: question title,
+        text, // You have a new Rocket available. Follow this link to blast off https://webaddress/cohort/question/student
+        html, // {text} <a href={link} />
         cc,
     };
 };
-
-const generateEmailIfValid = emailGenerator => validator =>
+/**
+ *
+ * @param {typeof __generateEmail} emailGenerator
+ */
+const generateEmailIfValid = emailGenerator =>
     /**
-     * Promise that generates email when valid, and returns
-     * errors array when invalid.
-     * @param {object} emailProps
-     * @param {string[]} emailProps.to All the recipients of this email
-     * @param {string} emailProps.from Email address that will be included in the From field
-     * @param {string} emailProps.replyTo Email address that will be included in the Reply field
-     * @param {string} emailProps.subject Text that will be included in the Subject field
-     * @param {string} emailProps.text String depiction of email content that will be shown when html is disabled
-     * @param {string} emailProps.html Rendered HTML when email content allows for html
-     * @param {string|null} [email.cc=null] If the property is not null, the email address that should be included in the CC. Defaults to `null`
+     * @param {typeof Validator} validator Promise that runs validation
      */
-    async emailProps => {
-        const { isValid, errors } = await validate(emailProps, validator);
-        return isValid ? emailGenerator(emailProps) : { errors };
-    };
+    validator =>
+        /**
+         * Promise that generates email when valid, and returns
+         * errors array when invalid.
+         * @param {object} emailProps
+         * @param {string[]} emailProps.to All the recipients of this email
+         * @param {string} emailProps.from Email address that will be included in the From field
+         * @param {string} emailProps.replyTo Email address that will be included in the Reply field
+         * @param {string} emailProps.subject Text that will be included in the Subject field
+         * @param {string} emailProps.text String depiction of email content that will be shown when html is disabled
+         * @param {string} emailProps.html Rendered HTML when email content allows for html
+         * @param {string|null} [email.cc=null] If the property is not null, the email address that should be included in the CC. Defaults to `null`
+         */
+        async emailProps => {
+            const { isValid, errors } = await validate(emailProps, validator);
+            return isValid ? emailGenerator(emailProps) : { errors };
+        };
 
+/**
+ * Promise that generates an email if valid email object, otherwise returns an object
+ * with errors.
+ */
 const generateEmail = generateEmailIfValid(__generateEmail)(Validator);
 
 module.exports = { generateEmail, validate, Validator };
