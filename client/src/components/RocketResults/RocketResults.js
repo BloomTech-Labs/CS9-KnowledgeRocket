accbfb1f7684fe0a7d9e500f786b056283312fa8
unknown <bsa6.23.94@gmail.com>
2018-08-29 13:51:12 -0400

    added map state to props to rocket results

--- client/src/components/RocketResults/RocketResults.js
+++ client/src/components/RocketResults/RocketResults.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
+function mapStateToProps(state) {
+    return {
+        state,
+    };
+}
 class RocketResult extends Component {
     render() {
         return <div />;
     }
 }
+
+export default connect(
+    mapStateToProps,
+    { generateBreadCrumbs }
+)(RocketResult);
