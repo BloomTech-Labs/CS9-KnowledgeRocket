5d85b7534fc490729ad5bf063aa4502166523429
Moises Dobarganes <designerexpert@gmail.com>
2018-08-29 14:33:25 -0400

    Fixes #75

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -108,9 +108,10 @@ export const deleteRocket = rocketId => async dispatch => {
 
 // COHORT ACTIONS
 export const addCohort = (cohort, id) => async dispatch => {
+    const packagedCohort = {...cohort, teacher: id};
     dispatch({ type: ADDING_COHORT });
     try {
-        let response = await axios.post(`${url}/api/cohort`, { cohort, id });
+        let response = await axios.post(`${url}/api/cohort`, {cohort: packagedCohort, id});
         dispatch({ type: ADD_COHORT, payload: response.data });
     } catch (err) {
         dispatch({ type: ADD_COHORT_FAILURE });
