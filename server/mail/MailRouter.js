2eb9383441464dda6245f0d9aa1fa0414bd0135d
axhon <axhonruiz@gmail.com>
2018-08-29 13:42:04 -1000

    extract important values out of data

--- server/mail/MailRouter.js
+++ server/mail/MailRouter.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const { getAllCohorts, whereCohortRocket, getTodayAndTomorrow } = require('./MailModel');
 
+const intervalDictionary = {
+    td: 'twoDay',
+    tw: 'twoWeek',
+    tm: 'twoMonth',
+};
+
 router.route('/').get(async (_, res) => {
     const { today, tomorrow } = getTodayAndTomorrow();
 
     const whereCohortIntervalIs = interval => whereCohortRocket(interval, today, tomorrow);
 
+    // The order of these promises is preserved once resolved
+    // https://stackoverflow.com/a/28066851
     const unresolvedCohortData = [
         getAllCohorts(whereCohortIntervalIs('td')),
         getAllCohorts(whereCohortIntervalIs('tw')),
@@ -14,7 +22,15 @@ router.route('/').get(async (_, res) => {
 
     try {
         const data = await Promise.all(unresolvedCohortData);
-        res.json(data);
+        // get all the emails
+        const emails = data.map(resolvedCohorts =>
+            resolvedCohorts.map(({ teacher, students: { email }, rockets }) => ({
+                teacher,
+                email,
+                rockets,
+            }))
+        );
+        res.json({ emails });
     } catch (err) {
         res.json({
             err,
