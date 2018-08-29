28b85bd06becf8f2fe3f74835a37dd0e9696de27
Axhon <axhon@users.noreply.github.com>
2018-08-29 13:35:24 -1000

    removes unused variable

--- client/src/components/CohortRocketList/CohortRocketList.js
+++ client/src/components/CohortRocketList/CohortRocketList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import CohortRocketCard from '../CohortRocketCard/CohortRocketCard';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Styled from 'styled-components';
