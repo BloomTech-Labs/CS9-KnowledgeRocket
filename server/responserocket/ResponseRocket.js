532c909178b9a77c089345a3b23eba38b36d08c7
unknown <bsa6.23.94@gmail.com>
2018-08-29 16:23:21 -0400

    response rocket possible retrieval method comments

--- server/responserocket/ResponseRocket.js
+++ server/responserocket/ResponseRocket.js
@@ -7,23 +7,40 @@ const ResponseRocket = mongoose.Schema({
     // Update it to an object with choice and date once submitted.
     // if it is not null populate the updated field instead of the submitted field.
     // * students: { 'id':studentId, answer: [{ choice: 0, submitted: date, updated: Date }]},
-    students: [{
-        studentId: {
-            type: ObjectId, ref: 'Student',
+    students: [
+        {
+            studentId: {
+                type: ObjectId,
+                ref: 'Student',
+            },
+            answer: [
+                {
+                    choice: {
+                        // Choices are indices for the answers array.
+                        type: Number,
+                        default: 0,
+                    },
+                    submitted: {
+                        type: Date,
+                        default: Date.now,
+                    },
+                },
+            ],
         },
-        answer: [
-            {
-                choice: {
-                    // Choices are indices for the answers array.
-                    type: Number, default: 0,
-                },
-                submitted: {
-                    type: Date, default: Date.now,
-                },
-            }
-        ],
-    }],
+    ],
     sent: { type: Number, default: 0 },
 });
-
+/* 
+#Go inside cohort for that specific user, 
+get the cohort id,
+grab the list of students,
+grab the list of rockets that are assigned,
+#go inside the rocket db,
+find all rockets that match rockets from the cohort,
+grab all question ids,
+#get all response rockets
+compare the students id that match the student ids from the cohort,
+grab all the responses,
+send that data to the frontend{probably some state stuff}
+*/
 module.exports = mongoose.model('ResponseRocket', ResponseRocket);
