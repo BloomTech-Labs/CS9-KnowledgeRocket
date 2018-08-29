539ca5f0eef54f300a204034ce12a6c05e829b88
JustinSHong <jhong2009@gmail.com>
2018-08-29 09:13:02 -0700

    sucessfully made query to find a user by id

--- server/csv/CSVRouter.js
+++ server/csv/CSVRouter.js
@@ -1,8 +1,27 @@
-const router = require('express').Router;
+const router = require('express').Router();
 const Student = require('../student/Student');
 const Cohort = require('../cohort/Cohort');
 const User = require('../user/User');
 
 router.route('/').post(post);
 
-function post(req, res) {}
+function post(req, res) {
+	const { teacherID, cohortID, studentData } = req.body;
+	console.log(`TEACHERID ${JSON.stringify(teacherID)}`);
+	console.log(`COHORTID ${JSON.stringify(teacherID)}`);
+	console.log(`STUDENTDATA ${JSON.stringify(studentData)}`);
+
+	// find cohort by id
+	// add new students to Cohort.students
+
+	// find teacher by id
+	User.findById(teacherID)
+		.then(found => {
+			console.log(`SUCECSSFULLY FOUND A USER ${found}`);
+		})
+		.catch(err => {
+			res.status(500).json({ errorMessage: err.message });
+		});
+}
+
+module.exports = router;
