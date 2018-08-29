0859d056675ed88c383419375f49b25a1d223220
axhon <axhonruiz@gmail.com>
2018-08-29 07:50:21 -1000

    gettodayandtomorrow to get current time when route is hit

--- server/mail/MailRouter.js
+++ server/mail/MailRouter.js
@@ -1,25 +1,11 @@
 const router = require('express').Router();
-const { getAllCohorts } = require('./MailModel');
-
-const whereCohortRocket = (interval, start, end) => ({
-    rockets: {
-        $elemMatch: {
-            [interval]: {
-                $gte: start,
-                $lt: end,
-            },
-        },
-    },
-});
+const { getAllCohorts, whereCohortRocket, getTodayAndTomorrow } = require('./MailModel');
 
 router.route('/').get(async (_, res) => {
-    const today = new Date(Date.now());
-    today.setHours(0, 0, 0, 0);
-    const tomorrow = new Date(Date.now());
-    tomorrow.setHours(23, 59, 59, 59);
+    const { today, tomorrow } = getTodayAndTomorrow();
 
     try {
-        const data = await getAllCohorts(whereCohortRocket('tw', today, tomorrow));
+        const data = await getAllCohorts(whereCohortRocket('td', today, tomorrow));
         res.json(data);
     } catch (err) {
         res.json({
