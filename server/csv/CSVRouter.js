e4aef0113c2fe164fa51214c620295efb187d471
JustinSHong <jhong2009@gmail.com>
2018-08-29 09:22:45 -0700

    successfully made query to find a cohort by id

--- server/csv/CSVRouter.js
+++ server/csv/CSVRouter.js
@@ -11,13 +11,20 @@ function post(req, res) {
 	console.log(`COHORTID ${JSON.stringify(teacherID)}`);
 	console.log(`STUDENTDATA ${JSON.stringify(studentData)}`);
 
-	// find cohort by id
 	// add new students to Cohort.students
 
 	// find teacher by id
 	User.findById(teacherID)
 		.then(found => {
 			console.log(`SUCECSSFULLY FOUND A USER ${found}`);
+			// find cohort by id
+			Cohort.findById(cohortID)
+				.then(found => {
+					console.log(`SUCESSFULLY FOUND A COHORT ${found}`);
+				})
+				.catch(err => {
+					res.status(500).json({ errorMessage: err.message });
+				});
 		})
 		.catch(err => {
 			res.status(500).json({ errorMessage: err.message });
