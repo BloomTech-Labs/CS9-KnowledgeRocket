a01c8a3b91880baee9f10bf8fcd6a20e44fc6baf
unknown <bsa6.23.94@gmail.com>
2018-08-30 14:12:09 -0400

    pie chart functioning

--- client/src/components/RocketResults/RocketResults.js
+++ client/src/components/RocketResults/RocketResults.js
@@ -36,35 +36,82 @@ const rows = [
     createGraph('TWO MONTH TEST QUESTION', 99, 83, 83),
 ];
 class RocketResult extends Component {
+    state = { canvas: '', ctx: '' };
     componentDidMount() {
         this.props.generateBreadCrumbs('/rocket/results');
+
+        // this.setState({
+        //     canvas,
+        //     ctx,
+        // });
+        this.piChart();
     }
+    piChart = () => {
+        //  ctx.arc(200 + offsetX, 200 + offsetY, 120(dont forget the offset), beginAngle, endAngle);
+        let choices = [22, 33, 12, 35];
+        let canvas = this.refs.canvas;
+        let ctx = canvas.getContext('2d');
+        let sum = 0;
+        for (let i = 0; i < choices.length; i++) {
+            sum += choices[i];
+        }
+        let colors = ['red', 'green', 'blue', 'orange'];
+        let bias = 0.15;
+        let offset = 8;
+        let beginAngle = 0;
+        let endAngle = 0;
+        let offsetX, offsetY, medianAngle;
+        for (let i = 0; i < choices.length; i++) {
+            //Converting to radians aka percentages of 2
+            let radians = Math.PI * ((choices[i] / sum) * 2);
+            beginAngle = endAngle + bias;
+            endAngle = endAngle + radians;
+            medianAngle = (endAngle + beginAngle) / 2;
+            offsetX = Math.cos(medianAngle) * offset;
+            offsetY = Math.sin(medianAngle) * offset;
+            ctx.beginPath();
+            ctx.fillStyle = colors[i % colors.length];
+            ctx.moveTo(200 + offsetX, 200 + offsetY);
+            ctx.arc(200, 200, 60 + offset * 2, beginAngle, endAngle);
+            ctx.lineTo(200 + offsetX, 200 + offsetY);
+            ctx.stroke();
+            ctx.fill();
+        }
+        ctx.fillstyle = 'white';
+    };
     render() {
         return (
-            <Paper>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Some Name for this Data likely a template literal</TableCell>
-                            <TableCell numeric>Participation</TableCell>
-                            <TableCell numeric>Sent</TableCell>
-                            <TableCell numeric>Students</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map(row => {
-                            return (
-                                <TableRow key={row.id}>
-                                    <TableCell>{row.label}</TableCell>
-                                    <TableCell numeric>{row.participation}%</TableCell>
-                                    <TableCell numeric>{row.sent}</TableCell>
-                                    <TableCell numeric>{row.students}</TableCell>
-                                </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-            </Paper>
+            <div>
+                <Paper>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>
+                                    Some Name for this Data likely a template literal
+                                </TableCell>
+                                <TableCell numeric>Participation</TableCell>
+                                <TableCell numeric>Sent</TableCell>
+                                <TableCell numeric>Students</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map(row => {
+                                return (
+                                    <TableRow key={row.id}>
+                                        <TableCell>{row.label}</TableCell>
+                                        <TableCell numeric>{row.participation}%</TableCell>
+                                        <TableCell numeric>{row.sent}</TableCell>
+                                        <TableCell numeric>{row.students}</TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </Paper>
+                <Paper>
+                    <canvas ref="canvas" width="600" height="500" />
+                </Paper>
+            </div>
         );
     }
 }
