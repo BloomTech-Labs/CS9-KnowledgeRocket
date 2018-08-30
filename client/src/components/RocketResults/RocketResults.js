a765dff27cd6d24a4ecd8c60c3cdac13fd032f0b
unknown <bsa6.23.94@gmail.com>
2018-08-30 17:38:45 -0400

    added some styling to rocket results

--- client/src/components/RocketResults/RocketResults.js
+++ client/src/components/RocketResults/RocketResults.js
@@ -45,7 +45,7 @@ class RocketResult extends Component {
         //     canvas,
         //     ctx,
         // });
-        this.piChart();
+        // this.piChart();
     }
     piChart = () => {
         //  ctx.arc(200 + offsetX, 200 + offsetY, 120(dont forget the offset), beginAngle, endAngle);
@@ -98,28 +98,34 @@ class RocketResult extends Component {
         let cumulativePercent = 0;
         for (let i = 0; i < choices.length; i++) {
             //Converting to radians aka percentages of 2
-            let offset = 0
+            let offset = 0;
             let offsetCenter = 1;
             let percent = choices[i] / sum;
             const startX = Math.cos(2 * Math.PI * (cumulativePercent + offset));
             const startY = Math.sin(2 * Math.PI * (cumulativePercent + offset));
             cumulativePercent += percent;
-            const endX = Math.cos(2 * Math.PI * (cumulativePercent));
-            const endY = Math.sin(2 * Math.PI * (cumulativePercent));
+            const endX = Math.cos(2 * Math.PI * cumulativePercent);
+            const endY = Math.sin(2 * Math.PI * cumulativePercent);
             const largeArcFlag = percent > 0.5 ? 1 : 0;
             const pathData = [
                 `M 0 0`,
                 `L ${startX} ${startY}`,
-                `M ${startX} ${startY}`,// Move
+                `M ${startX} ${startY}`, // Move
                 `A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`, // Arc
                 `L 0 0`, // Line
             ].join(' ');
-            svgs.push(<path d={pathData} fill={colors[i % colors.length]} className='pieChartSection'/>);
+            svgs.push(
+                <path d={pathData} fill={colors[i % colors.length]} className="pieChartSection" />
+            );
         }
 
         return (
             // SVG VIEWBOX 1:2 ratio -1 -1 2 2 were originals
-            <svg viewBox="-1.5 -1.5 3 3" style={{transform: 'rotate(-0.25turn)'}} space="preserve">
+            <svg
+                viewBox="-1.5 -1.5 3 3"
+                style={{ transform: 'rotate(-0.25turn)' }}
+                space="preserve"
+            >
                 {svgs.map(each => {
                     return each;
                 })}
@@ -128,8 +134,11 @@ class RocketResult extends Component {
     };
     render() {
         return (
-            <div><div style={{width: '300px', height: '300px', padding: '1rem'}}>{this.generateChart()}</div>
-                
+            <div>
+                <div style={{ width: '300px', height: '300px', padding: '1rem' }}>
+                    {this.generateChart()}
+                </div>
+
                 <Paper>
                     <Table>
                         <TableHead>
