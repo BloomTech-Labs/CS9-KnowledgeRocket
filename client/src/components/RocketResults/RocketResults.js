c60df6ca2ef51159cb9fa245ca3e54d05f2c48aa
Moises Dobarganes <designerexpert@gmail.com>
2018-08-30 16:26:42 -0400

    Implemented SVG for Rocket Results

--- client/src/components/RocketResults/RocketResults.js
+++ client/src/components/RocketResults/RocketResults.js
@@ -8,6 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { generateBreadCrumbs } from '../../actions';
+import './RocketResults.css';
 
 function mapStateToProps(state) {
     return {
@@ -79,9 +80,56 @@ class RocketResult extends Component {
         }
         ctx.fillstyle = 'white';
     };
+
+    generateChart = () => {
+        const svgs = [];
+        let choices = [22, 33, 12, 35];
+        let sum = 0;
+        for (let i = 0; i < choices.length; i++) {
+            sum += choices[i];
+        }
+        let colors = ['red', 'green', 'blue', 'orange'];
+        // let bias = 0.15;
+        // let offset = 8;
+        // let beginAngle = 0;
+        // let endAngle = 0;
+        // let offsetX, offsetY, medianAngle;
+
+        let cumulativePercent = 0;
+        for (let i = 0; i < choices.length; i++) {
+            //Converting to radians aka percentages of 2
+            let offset = 0
+            let offsetCenter = 1;
+            let percent = choices[i] / sum;
+            const startX = Math.cos(2 * Math.PI * (cumulativePercent + offset));
+            const startY = Math.sin(2 * Math.PI * (cumulativePercent + offset));
+            cumulativePercent += percent;
+            const endX = Math.cos(2 * Math.PI * (cumulativePercent));
+            const endY = Math.sin(2 * Math.PI * (cumulativePercent));
+            const largeArcFlag = percent > 0.5 ? 1 : 0;
+            const pathData = [
+                `M 0 0`,
+                `L ${startX} ${startY}`,
+                `M ${startX} ${startY}`,// Move
+                `A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY}`, // Arc
+                `L 0 0`, // Line
+            ].join(' ');
+            svgs.push(<path d={pathData} fill={colors[i % colors.length]} className='pieChartSection'/>);
+        }
+
+        return (
+            // SVG VIEWBOX 1:2 ratio -1 -1 2 2 were originals
+            <svg viewBox="-1.5 -1.5 3 3" style={{transform: 'rotate(-0.25turn)'}} space="preserve">
+                {svgs.map(each => {
+                    return each;
+                })}
+            </svg>
+        );
+    };
     render() {
         return (
-            <div>
+            <div><div style={{width: '300px', height: '300px', padding: '1rem'}}>{this.generateChart()}</div>
+                
                 <Paper>
                     <Table>
                         <TableHead>
