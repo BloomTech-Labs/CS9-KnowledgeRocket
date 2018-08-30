6b6b29195f3d92fd1de73a1dd74f0ddb132d69b5
axhon <axhonruiz@gmail.com>
2018-08-30 12:37:44 -1000

    trim data

--- server/mail/MailRouter.js
+++ server/mail/MailRouter.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 const { getAllCohorts, whereCohortRocket, getTodayAndTomorrow } = require('./MailModel');
 
-const intervalDictionary = {
-    td: 'twoDay',
-    tw: 'twoWeek',
-    tm: 'twoMonth',
-};
+const intervalDictionary = ['twoDay', 'twoWeek', 'twoMonth'];
+const d = ['td', 'tw', 'tm'];
 
 router.route('/').get(async (_, res) => {
     const { today, tomorrow } = getTodayAndTomorrow();
@@ -23,13 +20,27 @@ router.route('/').get(async (_, res) => {
     try {
         const data = await Promise.all(unresolvedCohortData);
         // get all the emails
-        const emails = data.map(resolvedCohorts =>
-            resolvedCohorts.map(({ teacher, students: { email }, rockets }) => ({
-                teacher,
-                email,
-                rockets,
-            }))
+        const emails = data.map((resolvedCohorts, i) =>
+            resolvedCohorts.map(({ teacher: { email }, students, rockets, cc }) => {
+                const studentsEmails = students.map(({ email, _id }) => ({ email, id: _id }));
+                const selectedRockets = rockets
+                    .filter(rocket => new Date(rocket[d[i]]).toUTCString() === today.toUTCString())
+                    .map(({ rocketId, _id }) => ({
+                        cohortId: _id,
+                        _id: rocketId[intervalDictionary[i]]._id,
+                        title: rocketId[intervalDictionary[i]].title,
+                    }))
+                    .map(({ _id, title, cohortId }) => ({ id: _id, title, cohortId }));
+
+                return {
+                    teacherEmail: email,
+                    students: studentsEmails,
+                    selectedRockets,
+                    cc,
+                };
+            })
         );
+
         res.json({ emails });
     } catch (err) {
         res.json({
