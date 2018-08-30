1d01c64a51418ac7d29795bf2422c619be61063a
unknown <bsa6.23.94@gmail.com>
2018-08-30 17:43:15 -0400

    commented out pichart

--- client/src/components/RocketResults/RocketResults.js
+++ client/src/components/RocketResults/RocketResults.js
@@ -47,39 +47,39 @@ class RocketResult extends Component {
         // });
         // this.piChart();
     }
-    piChart = () => {
-        //  ctx.arc(200 + offsetX, 200 + offsetY, 120(dont forget the offset), beginAngle, endAngle);
-        let choices = [22, 33, 12, 35];
-        let canvas = this.refs.canvas;
-        let ctx = canvas.getContext('2d');
-        let sum = 0;
-        for (let i = 0; i < choices.length; i++) {
-            sum += choices[i];
-        }
-        let colors = ['red', 'green', 'blue', 'orange'];
-        let bias = 0.15;
-        let offset = 8;
-        let beginAngle = 0;
-        let endAngle = 0;
-        let offsetX, offsetY, medianAngle;
-        for (let i = 0; i < choices.length; i++) {
-            //Converting to radians aka percentages of 2
-            let radians = Math.PI * ((choices[i] / sum) * 2);
-            beginAngle = endAngle + bias;
-            endAngle = endAngle + radians;
-            medianAngle = (endAngle + beginAngle) / 2;
-            offsetX = Math.cos(medianAngle) * offset;
-            offsetY = Math.sin(medianAngle) * offset;
-            ctx.beginPath();
-            ctx.fillStyle = colors[i % colors.length];
-            ctx.moveTo(200 + offsetX, 200 + offsetY);
-            ctx.arc(200, 200, 60 + offset * 2, beginAngle, endAngle);
-            ctx.lineTo(200 + offsetX, 200 + offsetY);
-            ctx.stroke();
-            ctx.fill();
-        }
-        ctx.fillstyle = 'white';
-    };
+    // piChart = () => {
+    //     //  ctx.arc(200 + offsetX, 200 + offsetY, 120(dont forget the offset), beginAngle, endAngle);
+    //     let choices = [22, 33, 12, 35];
+    //     let canvas = this.refs.canvas;
+    //     let ctx = canvas.getContext('2d');
+    //     let sum = 0;
+    //     for (let i = 0; i < choices.length; i++) {
+    //         sum += choices[i];
+    //     }
+    //     let colors = ['red', 'green', 'blue', 'orange'];
+    //     let bias = 0.15;
+    //     let offset = 8;
+    //     let beginAngle = 0;
+    //     let endAngle = 0;
+    //     let offsetX, offsetY, medianAngle;
+    //     for (let i = 0; i < choices.length; i++) {
+    //         //Converting to radians aka percentages of 2
+    //         let radians = Math.PI * ((choices[i] / sum) * 2);
+    //         beginAngle = endAngle + bias;
+    //         endAngle = endAngle + radians;
+    //         medianAngle = (endAngle + beginAngle) / 2;
+    //         offsetX = Math.cos(medianAngle) * offset;
+    //         offsetY = Math.sin(medianAngle) * offset;
+    //         ctx.beginPath();
+    //         ctx.fillStyle = colors[i % colors.length];
+    //         ctx.moveTo(200 + offsetX, 200 + offsetY);
+    //         ctx.arc(200, 200, 60 + offset * 2, beginAngle, endAngle);
+    //         ctx.lineTo(200 + offsetX, 200 + offsetY);
+    //         ctx.stroke();
+    //         ctx.fill();
+    //     }
+    //     ctx.fillstyle = 'white';
+    // };
 
     generateChart = () => {
         const svgs = [];
