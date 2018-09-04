6f6a973830a6fbfe940d0cc66b8719e28cfd337b
Moises Dobarganes <designerexpert@gmail.com>
2018-09-04 15:23:02 -0400

    Attempt at fixing styles for Export CSV and Import CSV buttons

--- client/src/components/CohortSettingForm/CohortSettingForm.js
+++ client/src/components/CohortSettingForm/CohortSettingForm.js
@@ -18,10 +18,10 @@ function mapStateToProps(state) {
 }
 
 const styles = theme => ({
-	button: {
-		margin: theme.spacing.unit,
-		color: 'white',
-	},
+    button: {
+        margin: theme.spacing.unit,
+        color: 'white',
+    },
 });
 
 const StylizedInput = styled(Input)`
@@ -53,25 +53,23 @@ const StylizedCSVInput = styled.input`
     height: 0.1px;
 `;
 
-const ExportCSVBtn = styled(Button)`
-	background-color: #3f51b5 !important;
-`;
-
 const StyledLabel = styled.label`
-	box-sizing: border-box;
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: center;
-	color: white;
-	background-color: #3f51b5;
-	border-radius: 0.25rem;
-	padding: 0.6rem 0.8rem;
-	order: 1;
-	box-shadow: 0px 1px 3px 0px rgba(15, 12, 12, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
-		0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-	&:hover {
-		background-color: #303f9f;
-	}
+    box-sizing: border-box;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    color: white;
+    background-color: #3f51b5;
+    border-radius: 0.25rem;
+    padding: 1rem !important;
+    margin-right: 1rem;
+    margin-bottom: 1rem;
+    order: 1;
+    box-shadow: 0px 1px 3px 0px rgba(15, 12, 12, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+        0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+    &:hover {
+        background-color: #303f9f;
+    }
 `;
 
 const StyledFormControlLabel = styled(FormControlLabel)`
@@ -83,6 +81,10 @@ const StyledButton = styled(Button)`
     margin: 0 1rem 1rem 0rem !important;
     padding: 1rem !important;
     order: 4;
+`;
+
+const ExportCSVBtn = styled(StyledButton)`
+    background-color: #3f51b5 !important;
 `;
 
 // CONTAINS SETTINGS: CLASS NAME, CC CHECKBOX, IMPORT CSV
@@ -103,104 +105,106 @@ class CohortSettingForm extends Component {
         this.setState({ cohort });
     }
 
-	downloadCSV = () => {
-		// prepare csv data for download
-		const csv = 'data:text/csv;charset=utf-8,' + this.props.state.user.exportCSV;
-		const encodedUri = encodeURI(csv);
-		// create an empty link to allow user to save csv
-		const link = document.createElement('a');
-		link.setAttribute('href', encodedUri);
-		link.setAttribute('download', 'students.csv');
-		document.body.appendChild(link);
-		link.click();
-		document.body.removeChild(link);
-	};
+    downloadCSV = () => {
+        // prepare csv data for download
+        const csv = 'data:text/csv;charset=utf-8,' + this.props.state.user.exportCSV;
+        const encodedUri = encodeURI(csv);
+        // create an empty link to allow user to save csv
+        const link = document.createElement('a');
+        link.setAttribute('href', encodedUri);
+        link.setAttribute('download', 'students.csv');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
 
-	handleExportStudents = event => {
-		const cohortID = this.props.cohortID;
-		this.props
-			.exportCSV(cohortID)
-			.then(() => {
-				// allow user to download csv only after successful export event
-				this.downloadCSV();
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	};
+    handleExportStudents = event => {
+        const cohortID = this.props.cohortID;
+        this.props
+            .exportCSV(cohortID)
+            .then(() => {
+                // allow user to download csv only after successful export event
+                this.downloadCSV();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
 
-	handleFileSelect = event => {
-		const teacherID = this.props.state.user._id;
-		const cohortID = this.props.cohortID;
-		let studentData;
+    handleFileSelect = event => {
+        const teacherID = this.props.state.user._id;
+        const cohortID = this.props.cohortID;
+        let studentData;
 
-		const file = event.target.files[0];
-		const config = {
-			quoteChar: '"',
-			header: true,
-			preview: 0,
-			complete: (results, file) => {
-				studentData = results.data;
-				this.props.importCSV(teacherID, cohortID, studentData);
-				this.setState({ csvData: results });
-			},
-		};
-		if (file) {
-			return Papa.parse(file, config);
-		}
-	};
+        const file = event.target.files[0];
+        const config = {
+            quoteChar: '"',
+            header: true,
+            preview: 0,
+            complete: (results, file) => {
+                studentData = results.data;
+                this.props.importCSV(teacherID, cohortID, studentData);
+                this.setState({ csvData: results });
+            },
+        };
+        if (file) {
+            return Papa.parse(file, config);
+        }
+    };
 
-	handleCheckBox = (one, two) => {
-		this.setState({ cohort: { ...this.state.cohort, ccEmail: !this.state.cohort.ccEmail } });
-	};
+    handleCheckBox = (one, two) => {
+        this.setState({ cohort: { ...this.state.cohort, ccEmail: !this.state.cohort.ccEmail } });
+    };
 
-	handleNewInput = e => {
-		this.setState({ cohort: { ...this.state.cohort, [e.target.name]: e.target.value } });
-	};
+    handleNewInput = e => {
+        this.setState({ cohort: { ...this.state.cohort, [e.target.name]: e.target.value } });
+    };
 
-	handleAddCohort = () => {
-		const cohort = {
-			title: this.state.cohort.title,
-			ccEmail: this.state.cohort.ccEmail,
-		};
-		if (this.state.cohort._id !== '') {
-			cohort._id = this.state.cohort._id;
-		}
-		this.props.addCohort(cohort, this.props.state.user._id);
-	};
+    handleAddCohort = () => {
+        const cohort = {
+            title: this.state.cohort.title,
+            ccEmail: this.state.cohort.ccEmail,
+        };
+        if (this.state.cohort._id !== '') {
+            cohort._id = this.state.cohort._id;
+        }
+        this.props.addCohort(cohort, this.props.state.user._id);
+    };
 
-	render() {
-		const { classes } = this.props;
+    render() {
+        const { classes } = this.props;
 
-		return (
-			<div className={this.props.className}>
-				<StylizedInput
-					disableUnderline={true}
-					name="title"
-					onChange={this.handleNewInput}
-					onBlur={this.handleNewInput}
-					placeholder={this.state.cohort.title === '' ? 'Class Name' : this.state.cohort.title}
-				/>
-				<StylizedForm>
-					<StyledLabel for="csv-file">Import CSV</StyledLabel>
-					<StylizedCSVInput
-						type="file"
-						id="csv-file"
-						name="files"
-						onChange={this.handleFileSelect}
-					/>
-				</StylizedForm>
-				<ExportCSVBtn
-					variant="contained"
-					className={classes.button}
-					onClick={this.handleExportStudents}
-				>
-					Export CSV
-				</ExportCSVBtn>
+        return (
+            <div className={this.props.className}>
+                <StylizedInput
+                    disableUnderline={true}
+                    name="title"
+                    onChange={this.handleNewInput}
+                    onBlur={this.handleNewInput}
+                    placeholder={
+                        this.state.cohort.title === '' ? 'Class Name' : this.state.cohort.title
+                    }
+                />
+                <StylizedForm>
+                    <StyledLabel for="csv-file">Import CSV</StyledLabel>
+                    <StylizedCSVInput
+                        type="file"
+                        id="csv-file"
+                        name="files"
+                        onChange={this.handleFileSelect}
+                    />
+                </StylizedForm>
+                <ExportCSVBtn
+                    variant="contained"
+                    className={classes.button}
+                    onClick={this.handleExportStudents}
+                >
+                    Export CSV
+                </ExportCSVBtn>
                 <StyledFormControlLabel
                     control={
                         this.props.state.user.cohorts[this.props.cohortIDX].cc ? (
-                            <Checkbox onChange={this.handleCheckBox} name="ccEmail" checked/>
+                            <Checkbox onChange={this.handleCheckBox} name="ccEmail" checked />
                         ) : (
                             <Checkbox onChange={this.handleCheckBox} name="ccEmail" />
                         )
@@ -215,12 +219,15 @@ class CohortSettingForm extends Component {
                 >
                     Add / Edit Class
                 </StyledButton>
-			</div>
-		);
-	}
+            </div>
+        );
+    }
 }
 
-export default connect(mapStateToProps, {
-	importCSV,
-	exportCSV,
-})(withStyles(styles)(CohortSettingForm));
+export default connect(
+    mapStateToProps,
+    {
+        importCSV,
+        exportCSV,
+    }
+)(withStyles(styles)(CohortSettingForm));
