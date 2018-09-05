7bf8ae90e40c166899ad565afbe78c8f18b8ca5b
Moises Dobarganes <designerexpert@gmail.com>
2018-09-04 20:47:02 -0400

    Removed proptypes on Billing;

--- client/src/components/Billing/Billing.js
+++ client/src/components/Billing/Billing.js
@@ -10,7 +10,6 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
-import PropTypes from 'prop-types';
 // import Styled from 'styled-components';
 
 function mapStateToProps(state) {
