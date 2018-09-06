ccb58e0b80757d55b3196b08118bf7c4fe0a57c3
JustinSHong <jhong2009@gmail.com>
2018-09-05 17:14:46 -0700

    received and pass actionClick handler for triggering snackbar

--- client/src/components/CohortStudentList/CohortStudentList.js
+++ client/src/components/CohortStudentList/CohortStudentList.js
@@ -13,19 +13,26 @@ function mapStateToProps(state) {
 
 // CONTAINS THE LAST NAME, FIRST NAME, EMAIL, ADD BTN TO ADD STUDENTS
 class CohortStudentList extends Component {
-    componentDidMount() {
-    }
+    componentDidMount() {}
 
     render() {
         return (
             <Card className={this.props.className}>
-                {this.props.state.user.cohorts[this.props.cohortID].students.length === 0 ? 'Please add Students to your class' : null}
+                {this.props.state.user.cohorts[this.props.cohortID].students.length === 0
+                    ? 'Please add Students to your class'
+                    : null}
                 {/* Render all students added if the cohort exists with that index*/}
-                {this.props.state.user.cohorts[this.props.cohortID] ? this.props.state.user.cohorts[this.props.cohortID].students.map(
-                    (student, index) => (
-                        <CohortStudentCard student={student} key={`student_${index}`} />
-                    )
-                ): null}
+                {this.props.state.user.cohorts[this.props.cohortID]
+                    ? this.props.state.user.cohorts[this.props.cohortID].students.map(
+                          (student, index) => (
+                              <CohortStudentCard
+                                  student={student}
+                                  key={`student_${index}`}
+                                  trigger={this.props.actionClick}
+                              />
+                          )
+                      )
+                    : null}
             </Card>
         );
     }
