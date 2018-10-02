42974d4184435aaa3e286358dc0f3dff8aaa041d
JustinSHong <jhong2009@gmail.com>
2018-10-01 17:31:06 -0700

    auth page only shows Home on breadcrumb

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -231,21 +231,19 @@ class NavBar extends Component {
                         >
                             <MenuIcon />
                         </IconButton>
-                        {this.props.state.breadcrumb.labels.map((item, index) => {
-                            if (pathName !== '/rocket/auth') {
-                                <StyledCrumb key={item + index}>
-                                    <Link
-                                        to={this.props.state.breadcrumb.paths[index]}
-                                        style={{ textDecoration: 'none' }}
-                                    >
-                                        <Button style={{ color: 'white' }}> {item} </Button>
-                                    </Link>
-                                    <StyledSvg color="primary">
-                                        <path d="M5.59,7.41L7,6L13,12L7,18L5.59,16.59L10.17,12L5.59,7.41M11.59,7.41L13,6L19,12L13,18L11.59,16.59L16.17,12L11.59,7.41Z" />
-                                    </StyledSvg>
-                                </StyledCrumb>;
-                            }
-                        })}
+                        {this.props.state.breadcrumb.labels.map((item, index) => (
+                            <StyledCrumb key={item + index}>
+                                <Link
+                                    to={this.props.state.breadcrumb.paths[index]}
+                                    style={{ textDecoration: 'none' }}
+                                >
+                                    <Button style={{ color: 'white' }}> {item} </Button>
+                                </Link>
+                                <StyledSvg color="primary">
+                                    <path d="M5.59,7.41L7,6L13,12L7,18L5.59,16.59L10.17,12L5.59,7.41M11.59,7.41L13,6L19,12L13,18L11.59,16.59L16.17,12L11.59,7.41Z" />
+                                </StyledSvg>
+                            </StyledCrumb>
+                        ))}
                     </StyledBreadCrumbContainer>
                     <AccountSection authenticated={this.props.state.user.authenticated}>
                         <Avatar
